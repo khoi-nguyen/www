33035ce5792b24a720850b48be10e730382c2b0a
Khoi Nguyen <khoi@nguyen.me.uk>
2023-07-29 00:07:57 +0200

    Fix login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import { createCookieSessionStorage, redirect } from 'solid-start';
 const storage = createCookieSessionStorage({
   cookie: {
     httpOnly: true,
-    maxAge: 0,
+    maxAge: 30 * 24 * 3600,
     name: 'session',
     path: '/',
     sameSite: 'lax',
@@ -18,7 +18,7 @@ const fromRequest = (request: Request) => {
 
 export async function login(request: Request, password: string) {
   const session = await fromRequest(request);
-  const admin = password === process.env.ADMIN_PASSWORD || 'admin';
+  const admin = password === (process.env.ADMIN_PASSWORD || 'admin');
   session.set('admin', admin);
   return new Response(admin ? 'Logged in' : 'Authentication failed', {
     headers: {
