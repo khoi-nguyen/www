c49b4273249bf1f2e8af4fe6d129e4f6975f2625
Khoi Nguyen <khoi@nguyen.me.uk>
2023-07-29 20:09:05 +0200

    Separate general info in its own component

--- src/routes/teaching/2023-fall-algorithms/0-info.tsx
+++ src/routes/teaching/2023-fall-algorithms/0-info.tsx
@@ -1,44 +1,11 @@
 import meta from './0-info.json';
+import { Info } from './index.jsx';
 
 export default () => (
   <Slideshow meta={meta}>
     <Slide title="General Information">
       <p>Hello everyone! Welcome to Paris, I hope you'll have a fantastic term here.</p>
-      <dl>
-        <dt>Lectures</dt>
-        <dd>
-          <ul>
-            <li>Tuesday 17:45-19:00, 19:15-20:00</li>
-            <li>Thursday 17:45-19:00, 19:15-20:00</li>
-            <li>Lectures and recitations will be combined</li>
-          </ul>
-        </dd>
-        <dt>Resources</dt>
-        <dd>
-          <ul>
-            <li>
-              Annotated slides on <A href="/">https://nguyen.me.uk</A>
-            </li>
-            <li>
-              <Cite key="clrs" reference />
-            </li>
-          </ul>
-        </dd>
-        <dt>Email</dt>
-        <dd>
-          <a href="mailto:nguyen.khoi@nyu.edu">nguyen.khoi@nyu.edu</a>
-        </dd>
-        <dt>Office hour</dt>
-        <dd>Send me an email</dd>
-        <dt>Evaluation</dt>
-        <dd>
-          <ul>
-            <li>Homework: 50%</li>
-            <li>Midterm: 25%</li>
-            <li>Final: 25%</li>
-          </ul>
-        </dd>
-      </dl>
+      <Info />
     </Slide>
     <Slide title="Syllabus">
       <dl>
--- src/routes/teaching/2023-fall-algorithms/index.tsx
+++ src/routes/teaching/2023-fall-algorithms/index.tsx
@@ -1,25 +1,49 @@
 import meta from './index.json';
 
+export const Info = () => (
+  <dl>
+    <dt>Lectures</dt>
+    <dd>
+      <ul>
+        <li>Tuesday 17:45-19:00, 19:15-20:00</li>
+        <li>Thursday 17:45-19:00, 19:15-20:00</li>
+        <li>Lectures and recitations will be combined</li>
+      </ul>
+    </dd>
+    <dt>Resources</dt>
+    <dd>
+      <ul>
+        <li>
+          Annotated slides on <A href="/">https://nguyen.me.uk</A>
+        </li>
+        <li>
+          <Cite key="clrs" reference />
+        </li>
+      </ul>
+    </dd>
+    <dt>Email</dt>
+    <dd>
+      <a href="mailto:nguyen.khoi@nyu.edu">nguyen.khoi@nyu.edu</a>
+    </dd>
+    <dt>Office hour</dt>
+    <dd>Send me an email</dd>
+    <dt>Evaluation</dt>
+    <dd>
+      <ul>
+        <li>Homework: 50%</li>
+        <li>Midterm: 25%</li>
+        <li>Final: 25%</li>
+      </ul>
+    </dd>
+  </dl>
+);
+
 export default () => {
   const location = useLocation();
   return (
     <Page meta={meta}>
       <h2>Practical information</h2>
-      <dl>
-        <dt>Email</dt>
-        <dd>
-          <a href="mailto:nguyen.khoi@nyu.edu">nguyen.khoi@nyu.edu</a>
-        </dd>
-        <dt>Lectures</dt>
-        <dd>
-          <ul>
-            <li>Tuesday 17:45-19:00, 19:15-20:00</li>
-            <li>Thursday 17:45-19:00, 19:15-20:00</li>
-          </ul>
-        </dd>
-        <dt>Office Hour</dt>
-        <dd>TBD</dd>
-      </dl>
+      <Info />
       <h2>Teaching resources</h2>
       <Explorer pattern={`${location.pathname}/?[0-9]+-.*json$`} />
     </Page>
