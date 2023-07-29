de466e6fea2ab7e84b9d04627d0117f1504e3af9
Khoi Nguyen <khoi@nguyen.me.uk>
2023-07-29 11:40:18 +0200

    Fix breadcrumbs for home page

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,12 +1,15 @@
 export default function Breadcrumbs() {
   const location = useLocation();
   const parts = location.pathname.split('/').filter((path) => path);
+  if (parts.length === 0) {
+    parts.push('');
+  }
   const modules = import.meta.glob('~/routes/**/*.json');
   const [links] = createResource(async () => {
     return await Promise.all(
       parts.map(async (_, i) => {
         const path = '/' + parts.slice(0, i + 1).join('/');
-        const key: keyof typeof modules = [path, path + '/index']
+        const key: keyof typeof modules = [path, path + 'index', path + '/index']
           .map((s) => `/src/routes${s}.json`)
           .filter((p) => Object.keys(modules).includes(p))[0];
         const meta = (await modules[key]()) as Parameters<typeof Meta>[0];
@@ -19,23 +22,21 @@ export default function Breadcrumbs() {
   });
 
   return (
-    <Show when={!links.loading && links()!.length}>
-      <nav id="breadcrumbs">
-        <ul>
-          <li>
-            <A href="/">
-              <strong>nguyen.me.uk</strong>
-            </A>
-          </li>
-          <For each={links()}>
-            {(link) => (
-              <li>
-                <A href={link.path}>{link.title}</A>
-              </li>
-            )}
-          </For>
-        </ul>
-      </nav>
-    </Show>
+    <nav id="breadcrumbs">
+      <ul>
+        <li>
+          <A href="/">
+            <strong>nguyen.me.uk</strong>
+          </A>
+        </li>
+        <For each={links()}>
+          {(link) => (
+            <li>
+              <A href={link.path}>{link.title}</A>
+            </li>
+          )}
+        </For>
+      </ul>
+    </nav>
   );
 }
