1bc24481ffc5f8704a83541ee994fd64acb575ac
Khoi Nguyen <khoi@nguyen.me.uk>
2023-07-30 00:50:09 +0200

    Add link

--- src/bibliography.tsx
+++ src/bibliography.tsx
@@ -3,7 +3,10 @@ export default {
     'Cormen et al., 2022',
     <>
       Cormen, T. H., Leiserson, C. E., Rivest, R. L., & Stein, C. (2022).{' '}
-      <em>Introduction to algorithms</em>. MIT press.
+      <a href="https://en.wikipedia.org/wiki/Introduction_to_Algorithms">
+        <em>Introduction to algorithms</em>
+      </a>
+      . MIT press.
     </>,
   ],
 };
