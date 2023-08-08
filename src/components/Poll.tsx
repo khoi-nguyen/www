ae95530cfd9437a27ca26f2016e799fb74f64b3e
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-08 09:53:09 +0200

    Create BasicPoll component

--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -1,5 +1,35 @@
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons/index.js';
 
+interface BasicPollProps<T> {
+  children: JSX.Element | JSX.Element[] | string;
+  mark: (value: T) => Promise<boolean> | boolean;
+  value: T;
+}
+
+export const BasicPoll: Component<BasicPollProps<any>> = (props) => {
+  const [status, setStatus] = createSignal<'pending' | 'correct' | 'incorrect'>('pending');
+  createEffect(
+    on(
+      () => props.value,
+      async () => {
+        setStatus((await props.mark(props.value)) ? 'correct' : 'incorrect');
+      },
+      { defer: true },
+    ),
+  );
+
+  return (
+    <>
+      <div class={'poll ' + status()}>
+        {props.children}
+        <Show when={status() !== 'pending'}>
+          <Fa icon={status() === 'correct' ? faCheck : faXmark} />
+        </Show>
+      </div>
+    </>
+  );
+};
+
 interface PollProps {
   children: JSX.Element | JSX.Element[] | string;
   mark: (value: string) => Promise<boolean> | boolean;
@@ -7,32 +37,18 @@ interface PollProps {
 
 export default function Poll(props: PollProps) {
   const [value, setValue] = createSignal('');
-  const [status, setStatus] = createSignal<'pending' | 'correct' | 'incorrect'>('pending');
+  const [submittedValue, setSubmittedValue] = createSignal('');
   const handleInput: JSX.EventHandler<HTMLInputElement, InputEvent> = (event) => {
     setValue(event.currentTarget.value);
-  };
-  const handleSubmit = async () => {
-    const correct = await props.mark(value());
-    setStatus(correct ? 'correct' : 'incorrect');
   };
 
   return (
     <>
       {props.children}
-      <p class={'poll ' + status()}>
-        <input
-          type="text"
-          value={value()}
-          onInput={handleInput}
-          readonly={status() !== 'pending'}
-        />{' '}
-        <Show
-          when={status() !== 'pending'}
-          fallback={<input type="submit" onClick={handleSubmit} />}
-        >
-          <Fa icon={status() === 'correct' ? faCheck : faXmark} />
-        </Show>
-      </p>
+      <BasicPoll mark={props.mark} value={submittedValue()}>
+        <input type="text" value={value()} onInput={handleInput} />
+        <input type="submit" onClick={() => setSubmittedValue(value())} />
+      </BasicPoll>
     </>
   );
 }
