51bb4c9dd9af5336c2574ad8c6bc9344e593886f
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-12 00:37:00 +0200

    Add edition

--- src/bibliography.tsx
+++ src/bibliography.tsx
@@ -6,7 +6,7 @@ export default {
       <>
         Cormen, T. H., Leiserson, C. E., Rivest, R. L., &amp; Stein, C. (2022).{' '}
         <a href="https://en.wikipedia.org/wiki/Introduction_to_Algorithms">
-          <em>Introduction to algorithms</em>
+          <em>Introduction to algorithms, fourth edition</em>
         </a>
         . <Abbr key="MIT" /> press.
       </>
