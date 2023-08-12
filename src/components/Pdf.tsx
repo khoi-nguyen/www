e394ddc193a0a81532f23aa7403bc686bdfcd134
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-12 17:25:47 +0200

    Add page prop and change default values

--- src/components/Pdf.tsx
+++ src/components/Pdf.tsx
@@ -1,9 +1,14 @@
 interface PdfProps {
+  page?: number;
   src: string;
   width: string | number;
   height: string | number;
 }
 
 export default function Pdf(props: PdfProps) {
-  return <embed src={props.src} width={props.width} height={props.height} type="application/pdf" />;
+  props = mergeProps({ width: '100%', height: 500 }, props);
+  const src = () => {
+    return props.src + '#toolbar=0' + (props.page ? `&page=${props.page}` : '');
+  };
+  return <embed src={src()} width={props.width} height={props.height} type="application/pdf" />;
 }
