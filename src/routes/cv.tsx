97f6fca0d470bb166d60e8c1c722d8deaa3e00c0
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-20 23:04:44 +0200

    Add CEA position

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -80,6 +80,18 @@ export default () => (
     </Line>
 
     <Line
+      title="Lecturer in Mathematics"
+      employer={<a href="https://ceastudyabroad.com/">CEA Study Abroad</a>}
+      dates={[new Date('2023-05'), new Date('2023-06')]}
+    >
+      <ul>
+        <li>
+          <strong>Taught course:</strong> Differential Equations (for engineering students)
+        </li>
+      </ul>
+    </Line>
+
+    <Line
       title="Teacher of Mathematics and Computer Science"
       employer={<a href="https://britishschool.be">The British School of Brussels</a>}
       dates={[new Date('2021-08'), new Date('2023-01')]}
