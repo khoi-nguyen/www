40cd2ebcb85e0262c9b9d15dd57f3f99eba488fe
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-21 02:45:07 +0200

    Simplify teaching page

--- src/routes/teaching/index.tsx
+++ src/routes/teaching/index.tsx
@@ -1,19 +1,10 @@
 import meta from './index.json';
 
 export default () => {
-  const [search, setSearch] = createSignal('');
-
   return (
     <Page meta={meta}>
-      <div class="columns">
-        <h2>Courses</h2>
-        <div class="is-vcentered">
-          <label>
-            Search: <input value={search()} onInput={(event) => setSearch(event.target.value)} />
-          </label>
-        </div>
-      </div>
-      <Explorer pattern="teaching/.*/index.json$" filter={search()} />
+      <h2>Courses</h2>
+      <Explorer pattern="teaching/.*/index.json$" />
     </Page>
   );
 };
