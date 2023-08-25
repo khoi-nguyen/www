253923ccb7513687a7742e21d3415b10087fa9c0
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-25 14:47:36 +0200

    Improve flag component

--- src/components/Flag.tsx
+++ src/components/Flag.tsx
@@ -3,19 +3,20 @@ import { US, FR } from 'country-flag-icons/string/3x2';
 const flags = {
   en: US,
   fr: FR,
-};
+} as const;
 
 interface FlagProps {
-  country?: string;
-  lang: 'en' | 'fr';
+  country: string;
+  lang?: keyof typeof flags;
 }
 
 export default function Flag(props: FlagProps) {
   const flag = () => {
     if (props.country) {
       return props.country;
+    } else if (props.lang) {
+      return flags[props.lang];
     }
-    return flags[props.lang];
   };
   return <span class="icon" innerHTML={flag()} />;
 }
