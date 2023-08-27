f75827584391a69b3a559a4bf637b0691693f45e
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-28 00:45:45 +0200

    Add logo on ECAM page

--- src/routes/teaching/ecam.tsx
+++ src/routes/teaching/ecam.tsx
@@ -1,8 +1,12 @@
 import meta from './ecam.json';
+import logo from '~/assets/ecam.svg';
 
 export default () => {
   return (
     <Page meta={meta}>
+      <div class="has-text-centered">
+        <img src={logo} alt="ECAM Brussels Engineering School" style={{ 'max-height': '200px' }} />
+      </div>
       <Explorer
         filter={(page) => page.subtitle?.startsWith('ECAM') === true}
         pattern={`teaching/.*/index\.json$`}
