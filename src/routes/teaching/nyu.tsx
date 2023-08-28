9babaf3ecf5d60a626f95c0d62611be99fc3e43b
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-28 13:19:43 +0200

    Add NYU logo on teaching page

--- src/routes/teaching/nyu.tsx
+++ src/routes/teaching/nyu.tsx
@@ -1,8 +1,12 @@
 import meta from './nyu.json';
+import logo from '~/assets/nyu.svg';
 
 export default () => {
   return (
     <Page meta={meta}>
+      <div class="has-text-centered">
+        <img src={logo} alt="New York University" style={{ height: '100px' }} />
+      </div>
       <Explorer
         filter={(page) => page.subtitle?.startsWith('NYU') === true}
         pattern={`teaching/.*/index\.json$`}
