7a64f67b1013b855d67240c621372dd61737b0e5
Khoi Nguyen <khoi@nguyen.me.uk>
2023-08-31 13:35:23 +0200

    Create Geogebra component

---
+++ src/components/Geogebra.tsx
@@ -0,0 +1,24 @@
+interface GeogebraProps {
+  height?: number;
+  id: string;
+  width?: number;
+}
+
+export default function Geogebra(props: GeogebraProps) {
+  props = mergeProps({ height: 600, width: 900 }, props);
+  const src = () => {
+    let result = `https://www.geogebra.org/material/iframe/id/${props.id}`;
+    result += `/width/${props.width}/height/${props.height}`;
+    result += `/ai/false/smb/false/stb/false`;
+    return result;
+  };
+  return (
+    <iframe
+      class="clickable"
+      src={src()}
+      height={props.height}
+      width={props.width}
+      allowfullscreen
+    />
+  );
+}
