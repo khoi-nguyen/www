eb1b66939e69b6f2b2f0b46c6694200f938122a9
Khoi Nguyen <khoi@nguyen.me.uk>
2023-09-01 11:47:56 +0200

    Center youtube

--- src/components/Youtube.tsx
+++ src/components/Youtube.tsx
@@ -7,12 +7,15 @@ interface YoutubeProps {
 
 export default function Youtube(props: YoutubeProps) {
   return (
-    <iframe
-      width={(props.width || 560) * (props.zoom || 1)}
-      height={(props.height || 315) * (props.zoom || 1)}
-      src={props.src.replace('watch?v=', 'embed/')}
-      allow="autoplay; encrypted-media"
-      allowfullscreen
-    ></iframe>
+    <div class="has-text-centered block">
+      <iframe
+        class="clickable"
+        width={(props.width || 560) * (props.zoom || 1)}
+        height={(props.height || 315) * (props.zoom || 1)}
+        src={props.src.replace('watch?v=', 'embed/')}
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      />
+    </div>
   );
 }
