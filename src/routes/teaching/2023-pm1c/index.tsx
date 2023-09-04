6ac311b441e419fe55cf91df5277ff101bf3fa4a
Khoi Nguyen <khoi@nguyen.me.uk>
2023-09-05 00:51:39 +0200

    Add ECAM email

--- src/routes/teaching/2023-pm1c/index.tsx
+++ src/routes/teaching/2023-pm1c/index.tsx
@@ -5,6 +5,12 @@ export default () => {
   return (
     <Page meta={meta}>
       <h2>Informations pratiques</h2>
+      <dl>
+        <dt>Email</dt>
+        <dd>
+          <a href="mailto:ngy@ecam.be">ngy@ecam.be</a>
+        </dd>
+      </dl>
       <h2>Resources</h2>
       <Explorer pattern={`${location.pathname}/?[0-9]+-.*json$`} />
     </Page>
