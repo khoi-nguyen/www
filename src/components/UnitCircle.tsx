868092bacab12b66270e6b1d6021f8a60e85b2c1
Khoi Nguyen <khoi@nguyen.me.uk>
2023-09-13 12:05:20 +0200

    Make angle optional, add scale prop

--- src/components/UnitCircle.tsx
+++ src/components/UnitCircle.tsx
@@ -1,6 +1,7 @@
 interface UnitCircleProps {
-  angle: number;
+  angle?: number;
   label?: string;
+  scale?: number;
 }
 
 const tr = (x: number, axis: 'x' | 'y' = 'x') => {
@@ -9,38 +10,40 @@ const tr = (x: number, axis: 'x' | 'y' = 'x') => {
 
 export default function UnitCircle(props: UnitCircleProps) {
   const radius = 100;
-  const point = () => [tr(Math.cos(props.angle)), tr(Math.sin(props.angle), 'y')];
+  const point = () => [tr(Math.cos(props.angle || 0)), tr(Math.sin(props.angle || 0), 'y')];
   return (
     <div class="has-text-centered">
-      <svg viewBox="0 0 220 220" width={500}>
+      <svg viewBox="0 0 220 220" width={500 * (props.scale || 1)}>
         <circle cx={tr(0)} cy={tr(0)} fill="none" r={radius} stroke-width={2} stroke="black" />
         <line x1={tr(-1)} y1={tr(0)} x2={tr(1)} y2={tr(0)} stroke="black" stroke-width={2} />
         <line x1={tr(0)} y1={tr(-1)} x2={tr(0)} y2={tr(1)} stroke="black" stroke-width={2} />
-        <circle cx={point()[0]} cy={point()[1]} r={3} />
-        <line
-          x1={tr(0)}
-          y1={tr(0)}
-          x2={point()[0]}
-          y2={point()[1]}
-          stroke="black"
-          stroke-dasharray="2"
-        />
-        <line
-          x1={tr(0)}
-          y1={point()[1]}
-          x2={point()[0]}
-          y2={point()[1]}
-          stroke="black"
-          stroke-dasharray="2"
-        />
-        <line
-          x1={point()[0]}
-          y1={tr(0)}
-          x2={point()[0]}
-          y2={point()[1]}
-          stroke="black"
-          stroke-dasharray="2"
-        />
+        <Show when={props.angle}>
+          <circle cx={point()[0]} cy={point()[1]} r={3} />
+          <line
+            x1={tr(0)}
+            y1={tr(0)}
+            x2={point()[0]}
+            y2={point()[1]}
+            stroke="black"
+            stroke-dasharray="2"
+          />
+          <line
+            x1={tr(0)}
+            y1={point()[1]}
+            x2={point()[0]}
+            y2={point()[1]}
+            stroke="black"
+            stroke-dasharray="2"
+          />
+          <line
+            x1={point()[0]}
+            y1={tr(0)}
+            x2={point()[0]}
+            y2={point()[1]}
+            stroke="black"
+            stroke-dasharray="2"
+          />
+        </Show>
       </svg>
     </div>
   );
