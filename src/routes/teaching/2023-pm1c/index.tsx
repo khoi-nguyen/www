f7fbb1708de1bbed8cb7775ea5e3a6923df95060
Khoi Nguyen <khoi@nguyen.me.uk>
2023-09-14 08:50:57 +0200

    Change practical info

--- src/routes/teaching/2023-pm1c/index.tsx
+++ src/routes/teaching/2023-pm1c/index.tsx
@@ -6,9 +6,27 @@ export default () => {
     <Page meta={meta}>
       <h2>Informations pratiques</h2>
       <dl>
-        <dt>Email</dt>
+        <dt>Objectifs</dt>
         <dd>
-          <a href="mailto:ngy@ecam.be">ngy@ecam.be</a>
+          Rappel des notions du cours de mathématiques 6h du secondaire qui vous seront utiles
+        </dd>
+        <dt>Théorie</dt>
+        <dd>NGUYEN Khoi (NGY)</dd>
+        <dt>Exercices</dt>
+        <dd>
+          ANNENKOFF David (ANN), GUERRIERI Rolando (R5G), JOLY Marius (MJ5), MARCHAL Jonas (JM5),
+          NGUYEN Khoi (NGY)
+        </dd>
+        <dt>Resources</dt>
+        <dd>
+          <ul>
+            <li>
+              <Cite key="stewart" reference />
+            </li>
+            <li>
+              Slides annotés sur mon site web: <A href="/">https://nguyen.me.uk</A>
+            </li>
+          </ul>
         </dd>
       </dl>
       <h2>Resources</h2>
