98124286b07add7553a62c111834f3d2d7f05e97
Khoi Nguyen <khoi@nguyen.me.uk>
2023-10-26 20:59:14 +0200

    Improve title

--- src/routes/teaching/ecam.tsx
+++ src/routes/teaching/ecam.tsx
@@ -1,6 +1,24 @@
 import meta from './ecam.json';
 import logo from '~/assets/ecam.svg';
 
+type Metadata = Parameters<typeof Meta>[0];
+interface Page extends Metadata {
+  path: string;
+}
+
+const title = (page: Page) => {
+  let suffix = '';
+  if (page.path.endsWith('-b/')) {
+    suffix = ' (B)';
+  }
+  return (
+    <>
+      {page.title}
+      {suffix}
+    </>
+  );
+};
+
 export default () => {
   return (
     <Page meta={meta}>
@@ -10,7 +28,7 @@ export default () => {
       <Explorer
         filter={(page) => page.subtitle?.startsWith('ECAM') === true}
         pattern={`teaching/.*/index\.json$`}
-        showPath
+        title={title}
       />
     </Page>
   );
