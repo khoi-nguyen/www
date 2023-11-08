1f973aa30df65f6fd56cd0211204c282d6c659d3
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-08 19:49:19 +0100

    Start graphs part II

---
+++ src/routes/teaching/2023-fall-algorithms/6-graphs-part-ii.tsx
@@ -0,0 +1,10 @@
+import meta from './6-graphs-part-ii.json';
+
+export default () => {
+  return (
+    <Slideshow meta={meta}>
+      <Slide title="Shortest path problems"></Slide>
+      <Slide title="Dijkstra's algorithm"></Slide>
+    </Slideshow>
+  );
+};
