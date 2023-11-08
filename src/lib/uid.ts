1d3ef054263d5907c5da2d0e99144f24e1f63426
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-08 19:45:59 +0100

    Get user id

---
+++ src/lib/uid.ts
@@ -0,0 +1,11 @@
+export const userId = () => {
+  if (!localStorage.getItem('uid')) {
+    localStorage.setItem(
+      'uid',
+      '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: any) =>
+        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16),
+      ),
+    );
+  }
+  return localStorage.getItem('uid') as string;
+};
