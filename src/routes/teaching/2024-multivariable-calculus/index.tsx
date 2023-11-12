67b753f013a688e9f449642327caff304304d86a
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-12 16:54:00 +0100

    Add practical info for the course

--- src/routes/teaching/2024-multivariable-calculus/index.tsx
+++ src/routes/teaching/2024-multivariable-calculus/index.tsx
@@ -4,7 +4,24 @@ export default () => {
   const location = useLocation();
   return (
     <Page meta={meta}>
-      <h2>Resources</h2>
+      <h2>Informations pratiques</h2>
+      <dl>
+        <dt>Théorie</dt>
+        <dd>NGUYEN Khoi (NGY)</dd>
+        <dt>Exercices</dt>
+        <dd>HILLEWAERE Ruben (HIL), JONAS-SZATANSKI Jacek (JSZ), NGUYEN Khoi (NGY)</dd>
+        <dt>Ressources</dt>
+        <dd>
+          <ul>
+            <li>
+              <Cite key="stewart" reference />
+            </li>
+            <li>
+              Slides annotés sur mon site web: <A href="/">https://nguyen.me.uk</A>
+            </li>
+          </ul>
+        </dd>
+      </dl>
       <Explorer pattern={`${location.pathname}/?[0-9a-z]+-.*json$`} showPDF />
     </Page>
   );
