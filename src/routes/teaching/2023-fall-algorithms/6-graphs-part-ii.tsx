14ba18325c35c117d986baa561b01fc3debb4333
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-16 17:08:59 +0100

    Add Bellman-Ford

--- src/routes/teaching/2023-fall-algorithms/6-graphs-part-ii.tsx
+++ src/routes/teaching/2023-fall-algorithms/6-graphs-part-ii.tsx
@@ -129,6 +129,205 @@ export default () => {
       <Slide title="Exercises">
         <Iframe src="https://cims.nyu.edu/~regev/teaching/basic_algorithms_spring_2022/hw12.pdf" />
       </Slide>
+      <Slide title="Bellman-Ford">
+        <p>
+          Dijkstra is an efficient algorithm if the weights associated with the edges are{' '}
+          <strong>nonnegative</strong>.
+        </p>
+        <img
+          src="https://cdn.programiz.com/sites/tutorial2program/files/Bellman-Ford-Algorithm-1.png"
+          width="100%"
+        />
+        <Example>
+          <p>
+            Apply Dijkstra to this graph (we'll apply Bellman-Ford afterwards) with {tex`A`} as a
+            starting point
+          </p>
+        </Example>
+      </Slide>
+      <Slide title="Bellman-Ford: implementation">
+        <Exercise>
+          <p>
+            Implement Bellman-Ford to find the length of the shortest path. What's its time
+            complexity?
+          </p>
+        </Exercise>
+        <Jupyter
+          solution={py`
+            def bellman_ford(V, E, s):
+                d = { u: 0 if u == s else float('inf') for u in V }
+                for i in range(len(V) - 1):
+                    for u, v, w in E:
+                        # Relax edge
+                        dist_via_u = d[u] + w
+                        if dist_via_u < dist[v]:
+                            d[v] = dist_via_u
+                return d
+          `}
+          hideUntil={new Date('2023-11-17')}
+        >
+          {py`
+            def bellman_ford(V, E, s):
+                d = { u: 0 if u == s else float('inf') for u in V }
+                for i in range(len(V) - 1):
+                    for u, v, w in E:
+                        pass
+                return d
+          `}
+        </Jupyter>
+        <Exercise>
+          <p>How would you get the actual path as well?</p>
+        </Exercise>
+      </Slide>
+      <Slide title="Bellman-Ford: correctness">
+        <Theorem title="Correctness of Bellman-Ford">
+          <p>
+            Assume {tex`G`} has no negative-weight cycles that are reachable from {tex`s`}.
+          </p>
+          <p>At the end of the algorithm,</p>
+          {tex`
+            d[v] = \delta(s, v).
+          `}
+          <p>
+            for all vertices {tex`v`} that are reachable from {tex`s`}.
+          </p>
+        </Theorem>
+        <h4>Sketch proof</h4>
+        <ul>
+          <li>
+            Let {tex`\underbrace{v_0}_s, v_1, \dots, v_{k - 1}, \underbrace{v_k}_{v}`} be a shortest
+            path.
+          </li>
+          <li>{tex`k \leq \abs V - 1`}</li>
+          <li>
+            <strong>Loop invariant</strong>: at the beginning of each iteration
+            {tex`
+              d[v_0] = \delta(s, v_0) \quad
+              d[v_1] = \delta(s, v_1) \quad
+              \dots \quad
+              d[v_i] = \delta(s, v_i)
+            `}
+          </li>
+        </ul>
+      </Slide>
+      <Slide title="Detecting negative-weight cycles">
+        <Proposition>
+          <p>
+            A graph contains a negative cycle reachable from {tex`s`} if and only if one edge can be
+            relaxed at the end of Bellman-Ford.
+          </p>
+        </Proposition>
+        <h4>Sketch proof</h4>
+        <ul>
+          <li>{tex`\Leftarrow`}: previous correctness result</li>
+          <li>
+            {tex`\Rightarrow`}:
+            {tex`
+              \begin{align*}
+                \sum_{i = 1}^k d[v_i] &\leq \sum_{i = 1}^k \left(d[v_{i - 1}] + w(v_{i - 1}, v_i)\right)\\
+              \end{align*}
+            `}
+          </li>
+        </ul>
+      </Slide>
+      <Slide title="Detecting negative-weight cycles: implementation">
+        <Exercise>
+          <p>
+            Change your Bellman-Ford code so that it detects negative cycle reachable from the
+            source
+          </p>
+        </Exercise>
+        <Jupyter
+          solution={py`
+            def bellman_ford(V, E, s):
+                d = { u: 0 if u == s else float('inf') for u in V }
+                for i in range(len(V) - 1):
+                    for u, v, w in E:
+                        # Relax edge
+                        dist_via_u = d[u] + w
+                        if dist_via_u < dist[v]:
+                            d[v] = dist_via_u
+                return d
+          `}
+          hideUntil={new Date('2023-11-17')}
+        >
+          {py`
+            def bellman_ford(V, E, s):
+                pass
+          `}
+        </Jupyter>
+      </Slide>
+      <Slide title="Exercises">
+        <Iframe src="https://cims.nyu.edu/~regev/teaching/basic_algorithms_spring_2022/hw12.pdf" />
+      </Slide>
+      <Slide title="All Pairs Shortest Paths">
+        <Problem>
+          <p>
+            Find the shortest path between <strong>all pairs</strong> of vertices
+          </p>
+        </Problem>
+        <Question>
+          <p>
+            What would the time complexities be if we applied Bellman-Ford or Dijkstra and iterate
+            through all vertices for the source node?
+          </p>
+        </Question>
+        <p>
+          We'll try to use <strong>dynamic programming</strong> to get a better time complexity.
+        </p>
+      </Slide>
+      <Slide title="1st attempt: guess next-to-last vertex">
+        <dl>
+          <dt>Subproblem</dt>
+          <dd>
+            <code>DP(u, v, k)</code>: length of shortest path {tex`u \rightsquigarrow v`} using at
+            most {tex`k`} edges
+          </dd>
+          <dt>Guess</dt>
+          <dd>Next-to-last vertex</dd>
+          <dt>Base cases</dt>
+          <dd>{tex`\text{DP}(u, v, 0) = \begin{cases}0 &\text{if } u = v \\ +\infty\end{cases}`}</dd>
+          <dt>Recurrence</dt>
+          <dd>
+            {tex`
+              \text{DP}(u, v, k) = \min\left\{
+                \text{DP}(u, v, k - 1),
+                \min_{y \in V} \left[ DP(u, y, k - 1) + w(y, v) \right]
+              \right\}
+            `}
+          </dd>
+        </dl>
+      </Slide>
+      <Slide title="Python implementation of the first attempt">
+        <Jupyter>
+          {py`
+            cache = {}
+            V = [] # set of vertices
+            def APSP(u, v, k):
+                if (u, v, k) in cache:
+                    return cache[(u, v, k)]
+                if k == 0:
+                    ans = 0 if u == v else float('inf')
+                else:
+                    ans = APSP(u, v, k - 1)
+                    for y in V:
+                        ans = min(ans, APSP(u, y, k-1) + w[y, v])
+                cache[(u, v, k)] = ans
+                return ans
+          `}
+        </Jupyter>
+        <Exercise>
+          <p>What is the time complexity?</p>
+        </Exercise>
+      </Slide>
+      <Slide title="Combining DP and D&C">
+        <Question>
+          <p>
+            What if we guess the middle vertex instead of the penultimate? What would the time
+            complexity be?
+          </p>
+        </Question>
+      </Slide>
     </Slideshow>
   );
 };
