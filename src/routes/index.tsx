884eca58fdb8039e6a460669a6f4a2cd3e90a16b
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-17 15:02:18 +0100

    Simplify home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,23 +5,8 @@ export default () => (
     <p>
       Hi everyone! My name is <strong>Khôi Nguyễn</strong> and I'm a mathematician and a programmer
       who is passionate about education. Currently, I work at <Abbr key="ECAM" /> and{' '}
-      <Abbr key="NYU" /> as a lecturer in Mathematics and Computer Science. I focus on Web
-      development and the crossover between mathematics and computer science.
-    </p>
-    <p>
-      Previously, I taught Mathematics and Computer Science in international schools such as the{' '}
-      <a href="https://britishschool.be">The British School of Brussels</a> or{' '}
-      <a href="https://www.kingscollegeschools.org/">
-        King's College, The British School of Madrid
-      </a>
-      . And before that, I worked as a <Abbr key="PhD" /> student at{' '}
-      <a href="https://imperial.ac.uk">Imperial College</a>. My focus was on developing global
-      Pseudo-Differential Calculus on Lie groups.
-    </p>
-    <p>
-      The main purpose of this website is to host my <A href="/teaching">teaching resources</A> for
-      my students. Its source code is available on{' '}
-      <a href="https://github.com/khoi-nguyen/www">Github</a>.
+      <Abbr key="NYU" /> as a lecturer in Mathematics and Computer Science. The main purpose of this
+      website is to host my <A href="/teaching">teaching resources</A> for my students.
     </p>
     <h2>Useful Links</h2>
     <ul>
