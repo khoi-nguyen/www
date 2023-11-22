6441b2a7a53ea5bfbd55d03c3cb10b535eccfc58
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-22 10:26:45 +0100

    Change literals to be below whiteboard

--- src/lib/literals/html.tsx
+++ src/lib/literals/html.tsx
@@ -1,17 +1,33 @@
 import dedent from 'dedent-js';
 
 export const css = (strings: TemplateStringsArray, ...values: (string | number)[]) => {
-  return <Editor lang="css">{dedent(String.raw(strings, ...values))}</Editor>;
+  return (
+    <Editor lang="css" readOnly>
+      {dedent(String.raw(strings, ...values))}
+    </Editor>
+  );
 };
 
 export const js = (strings: TemplateStringsArray, ...values: (string | number)[]) => {
-  return <Editor lang="js">{dedent(String.raw(strings, ...values))}</Editor>;
+  return (
+    <Editor lang="js" readOnly>
+      {dedent(String.raw(strings, ...values))}
+    </Editor>
+  );
 };
 
 export const html5 = (strings: TemplateStringsArray, ...values: (string | number)[]) => {
-  return <Editor lang="html">{dedent(String.raw(strings, ...values))}</Editor>;
+  return (
+    <Editor lang="html" readOnly>
+      {dedent(String.raw(strings, ...values))}
+    </Editor>
+  );
 };
 
 export const ts = (strings: TemplateStringsArray, ...values: (string | number)[]) => {
-  return <Editor lang="ts">{dedent(String.raw(strings, ...values))}</Editor>;
+  return (
+    <Editor lang="ts" readOnly>
+      {dedent(String.raw(strings, ...values))}
+    </Editor>
+  );
 };
