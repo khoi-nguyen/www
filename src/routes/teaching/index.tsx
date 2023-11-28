2cd24162845152fbed63c3d8422c7c45cd988f1e
Khoi Nguyen <khoi@nguyen.me.uk>
2023-11-28 17:38:10 +0100

    Change Teaching page

--- src/routes/teaching/index.tsx
+++ src/routes/teaching/index.tsx
@@ -1,16 +1,31 @@
 import meta from './index.json';
 
 export default () => {
+  const [search, setSearch] = createSignal('');
   return (
     <Page meta={meta}>
+      <div class="columns is-vcentered">
+        <h1>Teaching</h1>
+        <div>
+          <input type="text" value={search()} onInput={(event) => setSearch(event.target.value)} />
+        </div>
+      </div>
       <h2>Current Courses</h2>
       <Explorer
         pattern="teaching/.*/index.json$"
-        filter={(page) => page.current === true}
+        filter={(page) =>
+          page.current === true && (!search() || page.title.indexOf(search()) !== -1)
+        }
         showFlags
       />
       <h2>Archive</h2>
-      <Explorer pattern="teaching/.*/index.json$" filter={(page) => !page.current} showFlags />
+      <Explorer
+        pattern="teaching/.*/index.json$"
+        filter={(page) =>
+          page.current === true && (!search() || page.title.indexOf(search()) !== -1)
+        }
+        showFlags
+      />
     </Page>
   );
 };
