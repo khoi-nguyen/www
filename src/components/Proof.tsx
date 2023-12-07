588b32f044a90e8d2370e8b64c50fd78e9b0d27d
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-07 15:07:03 +0100

    Add proof env

---
+++ src/components/Proof.tsx
@@ -0,0 +1,15 @@
+interface ProofProps {
+  children?: JSX.Element | JSX.Element[];
+  sketch?: boolean;
+}
+
+export default function Proof(props: ProofProps) {
+  return (
+    <details>
+      <summary class="clickable">
+        <Show when={props.sketch}>Sketch</Show> Proof
+      </summary>
+      {props.children}
+    </details>
+  );
+}
