ba2961e10062067e1fc6496f0004cd1bcb1a8876
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-07 18:58:07 +0100

    Start web

---
+++ src/routes/teaching/2023-web/8-mvc.tsx
@@ -0,0 +1,148 @@
+import meta from './8-mvc.json';
+
+export default () => {
+  return (
+    <Slideshow meta={meta}>
+      <Slide
+        title={() => (
+          <>
+            <Abbr key="ORM" />
+          </>
+        )}
+      >
+        <Environment name="Objectif">
+          <p>
+            Manipuler les entrées de la base de données comme <strong>objets</strong>.
+          </p>
+        </Environment>
+        <Example title="Créer un utilisateur avec un ORM">
+          {js`
+            const user = new User();
+            user.update({ name: "Tuxie", language: "es" })
+            user.save();
+          `}
+        </Example>
+        <Question pluralize>
+          <ul>
+            <li>Quelles sont les avantages et les inconvénients de cette approche?</li>
+            <li>Pourquoi emploie-t-on l'orienté objet?</li>
+          </ul>
+        </Question>
+      </Slide>
+      <Slide title="Un ORM simple">
+        <Instruction pluralize>
+          <Editor lang="bash">
+            {py`
+              git clone https://github.com/khoi-nguyen/LW3L-orm
+              npm install
+              npm run dev
+            `}
+          </Editor>
+        </Instruction>
+        <Remark>
+          <p>
+            Cet <Abbr key="ORM" /> est écrit à des fins didactiques.
+          </p>
+        </Remark>
+      </Slide>
+      <Slide title="Modèle: configuration">
+        <p>
+          La classe Modèle génère les requêtes SQL à votre place, mais elle a besoin de quelques
+          informations
+        </p>
+        <Editor>
+          {py`
+            class Model {
+              static table = "";
+              static primary = [];
+            }
+          `}
+        </Editor>
+        <p>
+          Ceci se fait par l'<strong>héritage</strong>:
+        </p>
+        <Editor>
+          {py`
+            class Task extends Model {
+              static table = "schema.tasks";
+              static primary = ["id"];
+            }
+
+            class User extends Model {
+              static table = "schema.users";
+              static primary = ["id"];
+            }
+          `}
+        </Editor>
+      </Slide>
+      <Slide title="Modèle: utilisation">
+        <Example title="Créer une entrée">
+          {js`
+            const task = new Task();
+            task.update({ task: "Feed Tuxie" });
+            await task.save();
+          `}
+        </Example>
+        <Example title="Supprimer une entrée">
+          {js`
+            await Task.delete({ task: "Feed Tuxie" });
+          `}
+        </Example>
+        <Example title="Modifier une entrée">
+          {js`
+            const task = Task.load({ id: 3 });
+            task.update({ task: "Buy fish" });
+            await task.save();
+          `}
+        </Example>
+        <Example title="Lister des entrées">
+          {js`
+            const tasks = Task.loadMany();
+          `}
+        </Example>
+      </Slide>
+      <Slide title="Code de model.js">
+        <Github repo="khoi-nguyen/LW3L-orm" path="models/Model.js" lang="js" />
+      </Slide>
+      <Slide title="MVC">
+        <Definition>
+          <p>
+            <Abbr key="MVC" /> est un design pattern qui sépare les <strong>modèles</strong> et les{' '}
+            <strong>vues</strong> des <strong>contrôleurs</strong>.
+          </p>
+        </Definition>
+        <dl>
+          <dt>Modèle</dt>
+          <dd>Données et logique business</dd>
+          <dt>Vue</dt>
+          <dd>Interface de l'utilisateur</dd>
+          <dt>Contrôleur</dt>
+          <dd>Orchestre la vue et le modèle</dd>
+        </dl>
+        <p>
+          L'objectif est d'écrire du code <strong>maintenable</strong> et{' '}
+          <strong>bien organisé</strong>.
+        </p>
+        <Remark>
+          <p>
+            Nous allons implémenter ce design pattern <strong>purement côté serveur</strong>. Ceci
+            est en opposition avec la tendance actuelle de faire beaucoup de travail côté client.
+          </p>
+        </Remark>
+      </Slide>
+      <Slide title="MVC en pratique">
+        <ul>
+          <li>
+            <code>models/</code>: un fichier par modèle {tex`\approx`} table
+          </li>
+          <li>
+            <code>views/</code>: templates EJS.
+          </li>
+          <li>
+            <code>controllers/</code>
+          </li>
+        </ul>
+      </Slide>
+    </Slideshow>
+  );
+};
---
+++ src/routes/teaching/2023-web/index.tsx
@@ -0,0 +1,74 @@
+import meta from './index.json';
+
+export default () => {
+  const location = useLocation();
+  return (
+    <Page meta={meta}>
+      <h1>Ressources</h1>
+      <Explorer pattern={`${location.pathname}/?[0-9a-z]+-.*json$`} showPDF />
+      <h2>Solution de certains exercices</h2>
+      <h3>Refuge animalier</h3>
+      {html5`
+      <form>
+        <label>Type de l'animal</label>
+        <select name="type" value="cat">
+          <option value="dog" selected="selected">chien</option>
+          <option value="cat">chat</option>
+        </select>
+        <label>Nom de l'animal</label>
+        <input name="name" type="text" />
+        <label>Âge de l'animal</label>
+        <input name="age" type="number" />
+        <button onclick="addAnimal()" type="button">Ajouter</button>
+      </form>
+      <ul id="animals">
+      </ul>
+      <p>La moyenne d'âge des chiens est <span id="dogMean">0</span></p>
+      <p>La moyenne d'âge des chats est <span id="catMean">0</span></p>
+
+      `}
+      {js`
+        // Fonction permettant de créer un objet spécial
+        // qui met automatiquement le DOM à jour à chaque changement
+        //
+        // Exemple d'utilisation:
+        //
+        // const age = createStateVar('age');
+        // age.value = 3 // L'élément avec id 'age' affiche 3
+        // age.value = 5 // L'élément avec id 'age' affiche 5
+        const createStateVar = (id, template = x => x) => ({
+          get value() {
+            return this.state;
+          },
+          set value(newVal) {
+            document.getElementById(id).innerHTML = template(this.state = newVal);
+          },
+        })
+
+        // Fonction décrivant comment afficher les animaux
+        const showAnimal = (animal) => \`<li>\${animal.name} (\${animal.type}), \${animal.age} ans</li>\`;
+
+        // État du programme
+        // Tout changement de ces variables sera automatiquement propagé au DOM
+        const animals = createStateVar('animals', (list) => list.map(showAnimal).join(''));
+        const dogMean = createStateVar('dogMean');
+        const catMean = createStateVar('catMean');
+
+        // Calcule la moyenne pour un type d'animal (dog/cat)
+        function mean(type) {
+          const filtered = animals.value.filter(animal => animal.type === type);
+          const sum = filtered.reduce((total, animal) => total + animal.age, 0);
+          return filtered.length ? sum / filtered.length : 0;
+        }
+
+        // Fonction qui ajoute les animaux à partir du formulaire
+        function addAnimal() {
+          const form = Object.fromEntries(new FormData(document.forms[0]))
+          animals.value = [...(animals.value || []), { ...form, age: parseInt(form.age) }];
+          dogMean.value = mean('dog')
+          catMean.value = mean('cat')
+        }
+      `}
+    </Page>
+  );
+};
