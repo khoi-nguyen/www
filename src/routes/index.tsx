599f57a06ca82a11ebf99d7c3dfd4c8e0ef415a2
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-12 09:46:47 +0100

    Revert "Minor change"
    This reverts commit cd303a86c4494c14b06e3f71e206ef2996805734.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,6 @@ export default () => (
       <Abbr key="NYU" /> as a lecturer in Mathematics and Computer Science. The main purpose of this
       website is to host my <A href="/teaching">teaching resources</A> for my students.
     </p>
-    <p>Bonjour</p>
     <h2>Useful Links</h2>
     <ul>
       <li>
