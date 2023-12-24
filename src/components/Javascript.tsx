0329e62ec01e5f3093dd8ed51575133d1df0faf3
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-24 17:45:26 +0100

    Explicitely specify versions

--- src/components/Javascript.tsx
+++ src/components/Javascript.tsx
@@ -14,8 +14,8 @@ export default function Javascript(props: JavascriptProps) {
   const code = () => {
     let value = `(async () => {`;
     if (props.react) {
-      value += `await import('https://unpkg.com/react@18/umd/react.development.js');`;
-      value += `await import('https://unpkg.com/react-dom@18/umd/react-dom.development.js');`;
+      value += `await import('https://unpkg.com/react@18.2.0/umd/react.development.js');`;
+      value += `await import('https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js');`;
     }
     value += props.children ? String(props.children) : String(props.code);
     if (props.react) {
