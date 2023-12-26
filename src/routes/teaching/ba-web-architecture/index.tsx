ad2424b69f165395e96881d5f8a0812be8a79a27
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-26 03:30:25 +0100

    Complete practical info for AW4C

--- src/routes/teaching/ba-web-architecture/index.tsx
+++ src/routes/teaching/ba-web-architecture/index.tsx
@@ -4,7 +4,22 @@ export default () => {
   const location = useLocation();
   return (
     <Page meta={meta}>
-      <h2>Informations pratiques</h2>
+      <div class="columns">
+        <div>
+          <h2>Informations pratiques</h2>
+          <dl>
+            <dt>Théorie</dt>
+            <dd>NGUYEN Khoi (NGY)</dd>
+            <dt>Labos</dt>
+            <dd>LURKIN Quentin (LUR), NGUYEN Khoi (NGY)</dd>
+            <dt>Local</dt>
+            <dd>1E04</dd>
+          </dl>
+        </div>
+        <div>
+          <Calendar filter={(event) => event.title.startsWith('AW4C')} />
+        </div>
+      </div>
       <h2>Ressources</h2>
       <Explorer pattern={`${location.pathname}/?[0-9a-z]+-.*json$`} showPDF />
     </Page>
