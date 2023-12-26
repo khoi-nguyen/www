cbb66ef4ac87daa41079b69dc97578baa6b38ef8
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-26 02:03:17 +0100

    Complete home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,37 @@ import meta from './index.json';
 
 export default () => (
   <Page meta={meta}>
+    <h2>About me</h2>
     <p>
-      Hi everyone! My name is <strong>Khôi Nguyễn</strong> and I'm a mathematician and a programmer
-      who is passionate about education. Currently, I work at <Abbr key="ECAM" /> and{' '}
-      <Abbr key="NYU" /> as a lecturer in Mathematics and Computer Science. The main purpose of this
-      website is to host my <A href="/teaching">teaching resources</A> for my students.
+      Hi everyone! My name is <strong>Khôi Nguyễn</strong> (he/him) and I'm a mathematician and a
+      programmer who is passionate about education. Currently, I work at{' '}
+      <a href="https://ecam.be">
+        <Abbr key="ECAM" />
+      </a>{' '}
+      and{' '}
+      <a href="https://nyu.edu">
+        <Abbr key="NYU" />
+      </a>{' '}
+      as a lecturer in Mathematics and Computer Science. The main purpose of this website is to host
+      my <A href="/teaching">teaching resources</A> for my students.
+    </p>
+    <p>
+      I studied pure mathematics at the <a href="https://cam.ac.uk">University of Cambridge</a> and
+      obtained my <Abbr key="PhD" /> from{' '}
+      <a href="https://imperial.ac.uk">Imperial College London</a> in 2016. I specialized in
+      Analysis and Differential Geometry, and was part of the effort to extend global
+      Pseudo-Differential Calculus to non-compact Lie Groups.
+    </p>
+    <p>
+      I started teaching in 2017, obtaining my teaching qualification from the{' '}
+      <a href="https://ox.ac.uk">University of Oxford</a> in 2019. Before becoming a lecturer, I
+      mostly worked as a Teacher of Mathematics in international schools such as{' '}
+      <a href="https://britishschool.be">The British School of Brussels</a> and{' '}
+      <a href="https://www.madrid-soto.kingscollegeschools.org/en">
+        King's College, The British School of Madrid
+      </a>
+      . I think a lot about how to use front-end technologies to better teach mathematics and
+      programming.
     </p>
     <h2>Useful Links</h2>
     <ul>
