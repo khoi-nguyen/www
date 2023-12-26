7745d57b228d22cfb7460ce672ab965aad066cb8
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-26 03:44:34 +0100

    Use correct locale

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,11 +1,17 @@
 import { Calendar } from '@fullcalendar/core';
 import type { EventImpl } from '@fullcalendar/core/internal';
+import enLocale from '@fullcalendar/core/locales/en-gb';
+import frLocale from '@fullcalendar/core/locales/fr';
 import icalendarPlugin from '@fullcalendar/icalendar';
 import listPlugin from '@fullcalendar/list';
+import { langSignal } from '~/lib/signals';
+
+const [lang] = langSignal;
 
 interface CalendarProps {
   filter?: (event: EventImpl) => boolean;
 }
+
 export default (props: CalendarProps) => {
   const host = (<div />) as HTMLDivElement;
 
@@ -14,6 +20,7 @@ export default (props: CalendarProps) => {
       height: 'auto',
       plugins: [listPlugin, icalendarPlugin],
       initialView: 'listMonth',
+      locale: lang() === 'fr' ? frLocale : enLocale,
       events: {
         url: 'https://calendar.ecam.be/ics/ngy',
         format: 'ics',
