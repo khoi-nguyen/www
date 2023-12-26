07602e0e549355dabba3a1b6d48dc6fad7fd4697
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-26 03:36:13 +0100

    Resources before practical info

--- src/routes/teaching/ba-web-architecture/index.tsx
+++ src/routes/teaching/ba-web-architecture/index.tsx
@@ -4,6 +4,8 @@ export default () => {
   const location = useLocation();
   return (
     <Page meta={meta}>
+      <h2>Ressources</h2>
+      <Explorer pattern={`${location.pathname}/?[0-9a-z]+-.*json$`} showPDF />
       <div class="columns">
         <div>
           <h2>Informations pratiques</h2>
@@ -20,8 +22,6 @@ export default () => {
           <Calendar filter={(event) => event.title.startsWith('AW4C')} />
         </div>
       </div>
-      <h2>Ressources</h2>
-      <Explorer pattern={`${location.pathname}/?[0-9a-z]+-.*json$`} showPDF />
     </Page>
   );
 };
