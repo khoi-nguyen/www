9737fd7902fa7b466e7c04ffd1cf4687016a04ed
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-28 15:49:12 +0100

    Start introduction

---
+++ src/routes/teaching/ba-web-architecture/0-introduction.tsx
@@ -0,0 +1,60 @@
+import meta from './0-introduction.json';
+
+export default () => {
+  return (
+    <Slideshow meta={meta}>
+      <Slide title="Slides" columns={true}>
+        <Iframe src="/teaching/" />
+        <div>
+          <Question>
+            <p>Où trouver les slides?</p>
+          </Question>
+          <ul>
+            <li>
+              Allez sur mon site: <A href="/">https://nguyen.me.uk</A>
+            </li>
+            <li>Teaching &gt; Architecture Web (business analysts)</li>
+          </ul>
+          <Remark>
+            <p>
+              Ne pas confondre avec le cours d'architecture web pour les ingénieurs en informatique
+            </p>
+          </Remark>
+        </div>
+      </Slide>
+      <Slide title="Contenu du cours">
+        <ol>
+          <li>Introduction</li>
+          <li>
+            <Abbr key="SPA" />
+          </li>
+        </ol>
+      </Slide>
+      <Slide
+        title={() => (
+          <>
+            Rappels: <Abbr key="HTML" /> et <Abbr key="CSS" />
+          </>
+        )}
+        split={false}
+      >
+        <Jupyter lang="html" columns>
+          {dedent`
+            <h1>Ceci est un titre</h1>
+            <p class="lead">Bonjour, ceci est un paragraphe</p>
+            <p>Ceci est un autre paragraphe.</p>
+
+            <style>
+              h1 {
+                color: red;
+              }
+              .lead {
+                color: blue;
+              }
+            </style>
+          `}
+        </Jupyter>
+      </Slide>
+    </Slideshow>
+  );
+};
