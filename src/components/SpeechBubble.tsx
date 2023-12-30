97672f2c68ed9d90618d53f90def1d73611c146a
Khoi Nguyen <khoi@nguyen.me.uk>
2023-12-30 21:00:13 +0100

    Add speech bubble component

---
+++ src/components/SpeechBubble.tsx
@@ -0,0 +1,25 @@
+import koi from '~/assets/koi.svg';
+import tuxie from '~/assets/tuxie.svg';
+
+const images = {
+  koi,
+  tuxie,
+} as const;
+
+interface SpeechBubbleProps {
+  children?: JSX.Element;
+  image?: keyof typeof images;
+  height?: number;
+}
+
+export default function SpeechBubble(props: SpeechBubbleProps) {
+  props = mergeProps({ image: 'tuxie' as const, height: 200 }, props);
+  return (
+    <div class="columns">
+      <div class="is-narrow">
+        <img src={images[props.image!]} alt={props.image} height={props.height} />
+      </div>
+      <div>{props.children}</div>
+    </div>
+  );
+}
