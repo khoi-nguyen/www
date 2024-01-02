4284f24c579bc52af266a4f295582db264d85898
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-02 18:01:34 +0100

    Improve iframe height calculation

--- src/components/Html.tsx
+++ src/components/Html.tsx
@@ -5,7 +5,16 @@ interface HtmlProps {
 }
 
 const style = (code: string) => dedent`
+  <!DOCTYPE html>
+  <html>
   <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      border: 1px solid white;
+    }
     body, input, textarea, button {
       font-size: 1.5em;
     }
@@ -17,11 +26,12 @@ const style = (code: string) => dedent`
     var ro = new ResizeObserver(entries => {
       for (let entry of entries) {
         const cr = entry.contentRect;
-        window.frameElement.style.height = cr.height + 30 + "px";
+        window.frameElement.style.height = cr.height + 5 + "px";
       }
     });
     ro.observe(document.body);
   </script>
+  </html>
 `
 
 export default function Html(props: HtmlProps) {
