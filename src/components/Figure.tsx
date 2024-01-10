a3567a8c20b50a051d7040ed71f531ccb435c606
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-10 19:26:04 +0100

    Add width/height props

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -1,13 +1,15 @@
 interface FigureProps {
   alt: string
   src: string
+  width?: number
+  height?: number
   children?: JSX.Element | JSX.Element[]
 }
 
 export default function Figure(props: FigureProps) {
   return (
     <div class="has-text-centered">
-      <img src={'/images/' + props.src} alt={props.alt} />
+      <img src={'/images/' + props.src} alt={props.alt} width={props.width} height={props.height} />
       <Show when={props.children}>
         <p>{props.children}</p>
       </Show>
