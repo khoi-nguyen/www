50a456ac6488381e6e4ee53c683e20a6d069c8e4
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-11 23:21:51 +0100

    Improve introduction

--- src/routes/teaching/ba-web-architecture/0-introduction.tsx
+++ src/routes/teaching/ba-web-architecture/0-introduction.tsx
@@ -67,6 +67,22 @@ export default () => {
           </Remark>
         </div>
       </Slide>
+      <Slide title="Attentes">
+        <ul>
+          <li>
+            Les cours théoriques ne sont <strong>pas des cours de programmation</strong>. Je vous
+            montrerai du code, mais l'objectif reste les concepts sous-jacents et non la syntaxe.
+          </li>
+          <li>
+            Vous aurez l'occasion de "programmer" en labo. Vous pouvez utiliser l'intelligence
+            artificielle, le but est de travailler votre compréhension des concepts.
+          </li>
+          <li>
+            J'essayerai de <strong>communiquer clairement les attentes de l'examen</strong> durant
+            le cours, mais également vers la fin du cours.
+          </li>
+        </ul>
+      </Slide>
       <Slide title="Motivations">
         <p>
           Le déroulement du cours et des labos sera planifié pour aborder et détailler les points
@@ -84,6 +100,20 @@ export default () => {
             </li>
           </ul>
         </Environment>
+      </Slide>
+      <Slide title="Que s'est-il passé?" columns>
+        <Figure src="kardashian.png" alt="Screenshots of Kim Kardashian's instagram" width={1100}>
+          <p>Captures d'écran de la page d'Instagram de Kim Kardashian au fil des années.</p>
+        </Figure>
+        <div>
+          <p>
+            <a href="https://archive.org">Wayback Machine</a> permet de voir un snapshot d'une page
+            web au fil du temps.
+          </p>
+          <Question>
+            <p>Que s'est-il passé en 2016 et en 2019?</p>
+          </Question>
+        </div>
       </Slide>
       <Slide title="Pourquoi une application web?">
         {/* Source: https://aws.amazon.com/what-is/web-application/ */}
@@ -109,8 +139,45 @@ export default () => {
           </ul>
         </Fragment>
       </Slide>
-      <Slide title={() => <Abbr key="DOM" />}>
-        <Figure src="dom-nodes-relationships.png" alt="Relations entre noeuds du DOM" />
+      <Slide title="The State of Developer Ecosystem 2023" columns>
+        <Figure src="state-dev-ecosystem-2023.png" alt="Use of technologies over time" height={800}>
+          <p>
+            Graphe montrant l'utilisation des langages de programmation les plus populaires au fil
+            des ans.
+          </p>
+        </Figure>
+        <div>
+          <Question>
+            <p>
+              Quelles sont les tendances principales au niveau du développement? Le développement
+              web est-il en hausse ou en baisse?
+            </p>
+          </Question>
+        </div>
+      </Slide>
+      <Slide title="Backend? Frontend?">
+        <Figure src="frontend-backend.png" alt="Front-end vs. Back-end" />
+      </Slide>
+      <Slide title="Les protagonistes principaux de ce cours">
+        <Figure src="frontend-languages.png" alt="HTML-CSS-JS" width="100%" />
+        <dl>
+          <dt>
+            <Abbr key="HTML" />
+          </dt>
+          <dd>Métadonnées, contenu et structure de la page</dd>
+          <dt>
+            <Abbr key="CSS" />
+          </dt>
+          <dd>Apparence</dd>
+          <dt>JavaScript</dt>
+          <dd>
+            <ul>
+              <li>Comportement interactif (front-end)</li>
+              <li>Communication frontend/backend après le rendu initial</li>
+              <li>Communication avec les fichiers et base de données (back-end)</li>
+            </ul>
+          </dd>
+        </dl>
       </Slide>
       <Slide
         title={() => (
@@ -120,6 +187,9 @@ export default () => {
         )}
         split={false}
       >
+        <p>
+          Pour créer une page web, il suffit de créer un fichier texte <code>.html</code>
+        </p>
         <h3>Liens utiles</h3>
         <ul>
           <li>
@@ -148,8 +218,49 @@ export default () => {
                 color: blue;
               }
             </style>
+            <!-- Ou mieux <link href="..." /> -->
           `}
         </Jupyter>
+        <p>
+          <strong>Vocabulaire</strong>: balise, attribut, valeur, sélecteur
+        </p>
+      </Slide>
+      <Slide title={() => <Abbr key="DOM" />}>
+        <Definition title={() => <Abbr key="DOM" />}>
+          <p>
+            Le <Abbr key="DOM" /> est la représentation en mémoire d'une page <Abbr key="HTML" />.
+          </p>
+        </Definition>
+        <Figure src="dom-and-html.png" alt="DOM representation of an HTML code" width="100%" />
+        <Remark>
+          <p>
+            Vous pouvez voir le <Abbr key="DOM" /> d'une page avec les outils de développement (F12)
+          </p>
+        </Remark>
+        <p>
+          <strong>Vocabulaire</strong>: noeud, enfant, parent
+        </p>
+      </Slide>
+      <Slide title="DOM Live Viewer">
+        <Iframe src="https://software.hixie.ch/utilities/js/live-dom-viewer/" />
+      </Slide>
+      <Slide title="Modification du DOM post-chargement">
+        <p>
+          Il est possible de modifier le <Abbr key="DOM" /> manuellement. Je ne devrais probablement
+          vous montrer cette astuce-là mais il est possible de lire les mots de passe sur les
+          ordinateurs de vos amis...
+        </p>
+        <label class="clickable">
+          Mot de passe super secret: <input type="password" value="bien essayé" />
+        </label>
+        <p>
+          Généralement, les modifications du <Abbr key="DOM" /> se font en JavaScript. Dans
+          l'exemple ci-dessous, cliquer sur le bouton "Play" introduit des changements du{' '}
+          <Abbr key="DOM" />.
+        </p>
+        {py.jupyter`
+          0.1 * 0.1
+        `}
       </Slide>
       <Slide
         title={() => (
@@ -172,10 +283,11 @@ export default () => {
           </p>
         </Remark>
       </Slide>
-      <Slide title="Javascript">
+      <Slide title="JavaScript">
         <p>
-          Le Javascript permet de changer la page sans la changer entièrement, ce qui permet une
-          meilleure expérience pour l'utilisateur.
+          Le JavaScript permet de <em>partiellement</em> changer la page (plus précisément le{' '}
+          <Abbr key="DOM" />
+          ), ce qui permet une meilleure expérience pour l'utilisateur.
         </p>
         {html.jupyter`
           <h1 id="title">Bonjour</h3>
@@ -189,6 +301,131 @@ export default () => {
             }
           </script>
         `}
+      </Slide>
+      <Slide title="Exemple: publicité">
+        {html.jupyter`
+          <h1>Bienvenue sur mon site personnel</h1>
+          <p>Pour récupérer les 5 euros que m'a coûté mon hébergeur,
+          je vous rajoute une publicité et vous tracke avec Google.</p>
+
+          <script>
+            const ad = document.createElement('div')
+            ad.innerHTML = '<p><em>Méchante publicité</em></p>'
+            document.body.appendChild(ad)
+          </script>
+          <!--
+            Souvent chargée de l'extérieur avec
+            <script src="..."></script>
+          -->
+        `}
+      </Slide>
+      <Slide
+        title={() => (
+          <>
+            Manipuler le <Abbr key="DOM" />
+          </>
+        )}
+      >
+        <ol>
+          <li>
+            Sélectionner le noeud
+            {js.hl`
+              let node = document.querySelector('.bonjour')
+            `}
+            <em>aussi:</em> getElementById, getElementsByClassNames, querySelectorAll, etc.
+          </li>
+          <li>
+            Modification du noeud
+            {js.hl`
+              node.innerText = "Hello"
+            `}
+          </li>
+          <li>
+            Enlever un noeud
+            {js.hl`
+              node.remove()
+            `}
+          </li>
+          <li>
+            Ajouter un noeud:
+            {js.hl`
+              let parent = document.querySelector('...')
+              parent.appendChild(node)
+            `}
+          </li>
+        </ol>
+        <Remark>
+          <p>
+            Il y a énormément de type de noeuds (e.g. un pour chaque balise) qui ont énormément de
+            propriétés. Pour plus d'infos, consulter la{' '}
+            <a href="https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model">
+              documentation
+            </a>
+            .
+          </p>
+        </Remark>
+      </Slide>
+      <Slide title="Événements">
+        <p>
+          Une partie importante du Javascript est de réagir aux actions et interactions de
+          l'utilisateur avec la page (scroll, clic sur un bouton, etc.)
+        </p>
+        {html.jupyter`
+          <button>Clic!</button>
+          <input />
+          <span></span>
+
+          <script>
+            let button = document.querySelector('button')
+            let input = document.querySelector('input')
+            let span = document.querySelector('span')
+
+            button.onclick = function() {
+              alert('Clic')
+            }
+            input.onblur = function() {
+              alert('Vous avez entré: ' + input.value)
+            }
+          </script>
+        `}
+      </Slide>
+      <Slide title="Asynchronisme" split={false}>
+        <p>
+          Parfois, une opération <strong>nécéssite du temps</strong>. Pour ne pas bloquer
+          l'interface, on peut mettre en pause une fonction jusqu'à ce qu'elle ait la réponse avec{' '}
+          <code>await</code> à condition qu'on la marque avec <code>async</code>.
+        </p>
+        <Jupyter lang="html" columns>
+          {html.raw`
+            <input />
+            <img />
+            <script>
+              let input = document.querySelector('input')
+              let img = document.querySelector('img')
+              async function changePokemon(name) {
+                const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + name)
+                const data = await res.json()
+                img.src = data.sprites.other['official-artwork']['front_default']
+              }
+              input.onblur = function() {
+                changePokemon(input.value)
+              }
+            </script>
+          `}
+        </Jupyter>
+      </Slide>
+      <Slide title="Réactivité (or lack thereof)">
+        {ts.jupyter`
+          let a = 2
+          let b = 2 * a
+
+          a = 5
+        `}
+        <Question>
+          <p>
+            À la fin du code, que vaut <code>b</code>?
+          </p>
+        </Question>
       </Slide>
     </Slideshow>
   )
