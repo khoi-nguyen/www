208c121df8d6663cd5dd596ad0c7f9250247a8f6
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-15 17:37:33 +0100

    Improve BA's first lab

--- src/routes/teaching/ba-web-architecture/lab-01-components.tsx
+++ src/routes/teaching/ba-web-architecture/lab-01-components.tsx
@@ -10,10 +10,11 @@ const CVLine = svelte.raw`
   <script>
     export let title = ''
     export let employer = ''
+    export let date = ''
   </script>
 
   <div>
-    <h5>{title}, {employer}</h5>
+    <h5>{title}, {employer} ({date})</h5>
     <slot />
   </div>
 `
@@ -27,21 +28,17 @@ interface HealthProps {
 function Health(props: HealthProps) {
   const value = () => props.hp / props.maxHealth
   return (
-    <div>
+    <div class="block">
       {props.name}
       <br />
-      HP: <meter value={value()} low={0.1} />
+      HP: <meter value={value()} low={0.2} />
       <br />
       {props.hp} / {props.maxHealth}
     </div>
   )
 }
 
-interface PokemonProps {
-  showCatch?: boolean
-}
-
-function Pokemon(props: PokemonProps) {
+function Pokemon() {
   const maxHealth = 416
   const [hp, setHp] = createSignal(maxHealth)
   const [attempts, setAttempts] = createSignal(0)
@@ -62,10 +59,12 @@ function Pokemon(props: PokemonProps) {
   return (
     <>
       <Show when={hp() > 0}>
-        <Health name="Mewtwo" hp={hp()} maxHealth={maxHealth} />
+        <Show when={!caught()} fallback={<p>The Pokemon has been caught</p>}>
+          <Health name="Mewtwo" hp={hp()} maxHealth={maxHealth} />
+        </Show>
         <Button onClick={slap}>Slap</Button>
       </Show>
-      <Show when={props.showCatch && !caught()}>
+      <Show when={!caught()}>
         <Button onClick={catchPokemon}>Catch</Button>
       </Show>
       <Button onClick={reset}>Reset</Button>
@@ -84,7 +83,7 @@ export default () => {
       <Slide title="Installer NPM">
         <Iframe src="https://nodejs.org" height={600} />
         <Instruction pluralize>
-          <ul>
+          <ol>
             <li>
               Allez sur le site web the <strong>nodejs</strong>:{' '}
               <a href="https://nodejs.org">http://nodejs.org</a>
@@ -93,16 +92,18 @@ export default () => {
               Téléchargez la version appropriée pour votre machine (la version <Abbr key="LTS" />{' '}
               suffit, il n'est pas nécéssaire de prendre la toute dernière version)
             </li>
-          </ul>
+          </ol>
         </Instruction>
       </Slide>
       <Slide title="Créer un projet Svelte">
-        <Instruction>
-          <ul>
+        <Instruction pluralize>
+          <ol>
             <li>Ouvrez le terminal</li>
             <li>
-              Executez la commande: <code>npm create vite@latest</code> avec les options ci-dessous
-              (vous pouvez appeler le projet comme vous le voulez):
+              Executez la commande:
+              <Editor lang="bash">{`npm create vite@latest`}</Editor>
+              en veillant bien à sélectionner <strong>Svelte</strong> et <strong>JavaScript</strong>
+              .
               <Figure src="svelte-vite-setup.png" alt="Screenshot of npm create vite" />
             </li>
             <li>
@@ -115,8 +116,110 @@ export default () => {
                 `}
               </Editor>
             </li>
-          </ul>
+          </ol>
         </Instruction>
+      </Slide>
+      <Slide title="Création d'un premier composant">
+        <Instruction>
+          <ol>
+            <li>
+              Créez un fichier <code>src/lib/HelloWorld.svelte</code>
+            </li>
+            <li>
+              Écrivez-y quelque chose comme
+              {svelte.jupyter`
+                <script>
+                  let name = 'world'
+                </script>
+                <p>Hello {name}</p>
+                <style>
+                  p {
+                    color: blue
+                  }
+                </style>
+              `}
+            </li>
+            <li>
+              Importez et utilisez le composant dans <code>src/App.svelte</code>:
+              {svelte.hl`
+              <script>
+                import HelloWorld from './lib/HelloWorld.svelte'
+              </script>
+
+              <HelloWorld />
+            `}
+            </li>
+          </ol>
+        </Instruction>
+      </Slide>
+      <Slide
+        title={() => (
+          <>
+            Premier exercice: <Abbr key="CV" />
+          </>
+        )}
+        columns={true}
+      >
+        <Exercise>
+          <p>
+            Écrivez un composant <code>CvLine</code> qui vous permettra ensuite d'écrire votre{' '}
+            <Abbr key="CV" />. Enjolivez-le avec du <Abbr key="CSS" />
+          </p>
+          <Jupyter lang="svelte" modules={{ CVLine }}>
+            {svelte.raw`
+              <h1>Curriculum Vitæ</h1>
+              <h2>Parcours académique</h2>
+              <CVLine 
+                title="Master en mathématiques"
+                employer="ECAM"
+                date=""
+              >
+                <ul>
+                  <li>Bonjour</li>
+                </ul>
+              </CVLine>
+            `}
+          </Jupyter>
+        </Exercise>
+        <div>
+          <h3>Rappels</h3>
+          {tex`
+            \texttt{<}\overbrace{\texttt{CVLine}}^{\text{composant}}\,
+            \overbrace{\texttt{title}}^{\text{prop}}=
+            \texttt{value}\texttt{>}
+            \underbrace{\dots}_{\text{slot}}
+            \texttt{</CVLine>}
+          `}
+          <ul>
+            <li>
+              Les composants sont définis par un fichier <code>.svelte</code> dans{' '}
+              <code>./src/lib/</code>
+            </li>
+            <li>
+              Les props se déclarent dans le composant avec le mot clé <code>export</code>:
+              {svelte.hl`
+                <script>
+                  export let title
+                  // Ici, on donne une valeur par défaut
+                  export let employer = 'ECAM'
+                </script>
+              `}
+            </li>
+            <li>
+              Dans le composant, la balise <code>{html.raw`<slot />`}</code> indique où injecter le
+              contenu du slot.
+            </li>
+          </ul>
+        </div>
+      </Slide>
+      <Slide title="Pokemon">
+        <Exercise>
+          <p>
+            Implémentez une version simplifiée d'une bataille avec un Pokemon sauvage. Au plus le
+            Pokemon a de PV, au plus il doit être difficile à capturer.
+          </p>
+        </Exercise>
+        <Pokemon />
       </Slide>
     </Slideshow>
   )
