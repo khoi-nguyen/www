1aefe2cfa21fadff4a351d4666d1b84eb2874dcc
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-16 15:33:08 +0100

    Allow left positioning

--- src/components/SpeechBubble.tsx
+++ src/components/SpeechBubble.tsx
@@ -10,16 +10,17 @@ interface SpeechBubbleProps {
   children?: JSX.Element
   image?: keyof typeof images
   height?: number
+  position?: 'right' | 'left'
 }
 
 export default function SpeechBubble(props: SpeechBubbleProps) {
-  props = mergeProps({ image: 'tuxie' as const, height: 200 }, props)
+  props = mergeProps({ image: 'tuxie', height: 200, position: 'right' } as const, props)
   return (
     <div class="columns">
       <div class="is-narrow">
         <img src={images[props.image!]} alt={props.image} height={props.height} />
       </div>
-      <div class="speech-bubble-right">{props.children}</div>
+      <div class={'speech-bubble-' + props.position}>{props.children}</div>
     </div>
   )
 }
