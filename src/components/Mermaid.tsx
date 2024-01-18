d0b419c296ef292e32ffef2317bac1f8f892b75c
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-18 10:35:33 +0100

    Improve mermaid: unique ID + remove unused DOM els

--- src/components/Mermaid.tsx
+++ src/components/Mermaid.tsx
@@ -1,22 +1,20 @@
+import { createUniqueId } from 'solid-js'
+
 interface MermaidProps {
   children: string | string[]
 }
 
 export default function Mermaid(props: MermaidProps) {
-  let element: HTMLPreElement
+  let element: HTMLDivElement
   const c = children(() => props.children)
   const [svg, setSvg] = createSignal('')
 
   onMount(async () => {
     const mermaid = (await import('mermaid')).default
     const code = String(c())
-    const { svg } = await mermaid.render('graphDiv', code)
+    const { svg } = await mermaid.render(createUniqueId(), code)
     setSvg(svg)
   })
 
-  return (
-    <pre ref={element!} innerHTML={svg()}>
-      {props.children}
-    </pre>
-  )
+  return <div ref={element!} innerHTML={svg()} />
 }
