b30a7a289f90f3e34ec4239235735c59a67b14da
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-19 10:58:06 +0100

    Add calendar on EM1C course page

--- src/routes/teaching/2024-multivariable-calculus/index.tsx
+++ src/routes/teaching/2024-multivariable-calculus/index.tsx
@@ -1,3 +1,5 @@
+import { EventImpl } from '@fullcalendar/core/internal'
+
 const meta: Metadata = {
   title: 'Outils mathématiques',
   subtitle: 'ECAM/EM1C-T1 (Q2)',
@@ -6,29 +8,46 @@ const meta: Metadata = {
   current: true,
 }
 
+function changeEvent(event: EventImpl) {
+  const type = event.title.indexOf('-T1-') > -1 ? 'Cours théorique' : 'Exercices'
+  const group = event.title.indexOf('1BA-A') > -1 ? 'A' : 'B'
+  event.setProp('title', `${type} [${group}]`)
+}
+
 export default () => {
   const location = useLocation()
   return (
     <Page meta={meta}>
-      <h2>Informations pratiques</h2>
-      <dl>
-        <dt>Théorie</dt>
-        <dd>NGUYEN Khoi (NGY)</dd>
-        <dt>Exercices</dt>
-        <dd>HILLEWAERE Ruben (HIL), JONAS-SZATANSKI Jacek (JSZ), NGUYEN Khoi (NGY)</dd>
-        <dt>Ressources</dt>
-        <dd>
-          <ul>
-            <li>
-              <Cite key="stewart" reference />
-            </li>
-            <li>
-              Slides annotés sur mon site web: <A href="/">https://nguyen.me.uk</A>
-            </li>
-          </ul>
-        </dd>
-      </dl>
       <Explorer pattern={`${location.pathname}/?[0-9a-z]+-.*json$`} showPDF />
+      <div class="columns">
+        <div>
+          <h2>Informations pratiques</h2>
+          <dl>
+            <dt>Théorie</dt>
+            <dd>NGUYEN Khoi (NGY)</dd>
+            <dt>Exercices</dt>
+            <dd>HILLEWAERE Ruben (HIL), JONAS-SZATANSKI Jacek (JSZ), NGUYEN Khoi (NGY)</dd>
+            <dt>Ressources</dt>
+            <dd>
+              <ul>
+                <li>
+                  <Cite key="stewart" reference />
+                </li>
+                <li>
+                  Slides annotés sur mon site web: <A href="/">https://nguyen.me.uk</A>
+                </li>
+              </ul>
+            </dd>
+          </dl>
+        </div>
+        <div>
+          <Calendar
+            changeEvent={changeEvent}
+            filter={(event) => event.title.startsWith('EM1C')}
+            showLocation
+          />
+        </div>
+      </div>
     </Page>
   )
 }
