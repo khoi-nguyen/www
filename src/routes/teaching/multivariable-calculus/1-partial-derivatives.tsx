de41b09336a2ee515cba4c4fe2b163d3fcb42567
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-22 12:50:47 +0100

    Start partial derivatives

--- src/routes/teaching/multivariable-calculus/1-partial-derivatives.tsx
+++ src/routes/teaching/multivariable-calculus/1-partial-derivatives.tsx
@@ -32,6 +32,58 @@ export default () => {
           plt.contourf(X, Y, Z, levels=[0, np.inf], colors=['gray'])
         `}
       </Slide>
+      <Slide title="Graphes: fonctions linéaires">
+        <p>
+          Une fonction de type {tex`f(x, y) = ax + by + c`} est appelée <strong>linéaire</strong>.
+        </p>
+        <p>
+          Pour esquisser leurs graphes, trouvez les <strong>intersections avec les axes</strong>.
+        </p>
+        <Example>
+          <p>Esquissez le graphe de la function {tex`f(x) = 6 - 3x - 2y`}</p>
+        </Example>
+      </Slide>
+      <Slide title="Dérivées partielles">
+        <Definition>
+          {tex`
+            \frac {\partial f} {\partial x} &\defeq
+            \lim_{h \to 0} \frac {f(x + h, y) - f(x, y)} h\\
+            \frac {\partial f} {\partial y} &\defeq
+            \lim_{h \to 0} \frac {f(x, y + h) - f(x, y)} h
+          `}
+        </Definition>
+        <Remark>
+          <p>
+            La dérivée partielle peut également se noter: {tex`\partial_x f, \partial_1 f`} ou
+            encore {tex`f_x`}
+          </p>
+        </Remark>
+      </Slide>
+      <Slide title="Dérivées partielles: guide pratique" cite={['stewart', 'p. 1002']}>
+        <p>Il suffit de traiter les autres variables comme constantes et de d</p>
+        <Example title="Dérivées partielles">
+          <ol>
+            <li>
+              Calculez {tex`f_x(2, 1)`}et {tex`f_y(2, 1)`} pour
+              {tex`
+                f(x, y) \defeq x^3 + x^2 y^3 - 2y^2
+              `}
+            </li>
+            <li>
+              Calculez les dérivées partielles de
+              {tex`
+                f(x, y) \defeq \sin \left( \frac x {1 + y} \right)
+              `}
+            </li>
+          </ol>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y = symbols("x y")
+          f = x**3 + x**2 * y**3 - 2 * y**2
+          diff(f, x).subs({x: 2, y: 1})
+        `}
+      </Slide>
     </Slideshow>
   )
 }
