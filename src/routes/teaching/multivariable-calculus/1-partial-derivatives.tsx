4b5c8ddddc9d07577103e3b0ef27832c8a38e15f
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-23 17:23:59 +0100

    Various improvements to partial der chapter

--- src/routes/teaching/multivariable-calculus/1-partial-derivatives.tsx
+++ src/routes/teaching/multivariable-calculus/1-partial-derivatives.tsx
@@ -33,7 +33,27 @@ export default () => {
           plt.contourf(X, Y, Z, levels=[0, np.inf], colors=['gray'])
         `}
       </Slide>
-      <Slide title="Graphes: fonctions linéaires">
+      <Slide title="Domaine et image" cite={['stewart', 'p. 973']}>
+        <Example>
+          <p>Trouvez le domaine et l'image de {tex`g(x, y) \defeq \sqrt{9 - x^2 - y^2}`}</p>
+        </Example>
+      </Slide>
+      <Slide title="Graphes" cite={['stewart', 'p. 975']}>
+        <Definition title="Graphe">
+          {tex`
+            G_f \defeq \{(x, y, f(x, y)) : (x, y) \in \dom f\}
+          `}
+        </Definition>
+        {py.plot`
+          x = y = np.linspace(-2, 2, 400)
+          X, Y = np.meshgrid(x, y)
+          Z = - X * Y * np.exp(-X**2 - Y**2)
+          fig = plt.figure(figsize =(14, 9))
+          ax = plt.axes(projection ='3d')
+          ax.plot_surface(X, Y, Z)
+        `}
+      </Slide>
+      <Slide title="Graphes: exemples" cite={['stewart', 'pp. 975-976']}>
         <p>
           Une fonction de type {tex`f(x, y) = ax + by + c`} est appelée <strong>linéaire</strong>.
         </p>
@@ -41,11 +61,48 @@ export default () => {
           Pour esquisser leurs graphes, trouvez les <strong>intersections avec les axes</strong>.
         </p>
         <Example>
-          <p>Esquissez le graphe de la function {tex`f(x) = 6 - 3x - 2y`}</p>
+          <p>Esquissez le graphe de la fonction {tex`f(x) = 6 - 3x - 2y`}</p>
+        </Example>
+        <p>
+          Certains exemples seront basés sur des courbes connues telles que les cercles ou les
+          paraboles.
+        </p>
+        <Example>
+          <p>Esquissez le graphe de la fonction {tex`g(x, y) \defeq \sqrt{9 - x^2 - y^2}`}</p>
         </Example>
       </Slide>
+      <Slide title="Courbes de niveau" cite={['stewart', 'pp. 977-978']}>
+        {py.jupyter`
+          import numpy as np
+          import matplotlib.pyplot as plt
+          x = y = np.linspace(-1, 1, 500)
+          X, Y = np.meshgrid(x, y)
+          Z = X**2 + Y**2
+          plt.contour(X, Y, Z, levels=10)
+        `}
+      </Slide>
+      <Slide title="Courbes de niveau: exemples" cite={['stewart', 'p. 980']}>
+        <Example>
+          <p>
+            Esquissez les courbes de niveau de la fonction {tex`f(x, y) \defeq 6 - 3x - 2y`} pour
+            les valeurs {tex`k = -6, 0, 6, 12`}
+          </p>
+        </Example>
+        <Example>
+          <p>
+            Esquissez les courbes de niveau de la fonction{' '}
+            {tex`g(x, y) \defeq \sqrt{9 - x^2 - y^2}`} pour les valeurs {tex`k = 0, 1, 2, 3`}
+          </p>
+        </Example>
+      </Slide>
+      <Slide title="Dérivées partielles: introduction">
+        <Idea>
+          <p>Regarder le comportement d'une fonction dans les directions parallèles aux axes.</p>
+        </Idea>
+        <Geogebra id="uHsShAac" />
+      </Slide>
       <Slide title="Dérivées partielles">
-        <Definition>
+        <Definition title="Dérivées partielles">
           {tex`
             \frac {\partial f} {\partial x} &\defeq
             \lim_{h \to 0} \frac {f(x + h, y) - f(x, y)} h\\
@@ -65,7 +122,7 @@ export default () => {
         <Example title="Dérivées partielles">
           <ol>
             <li>
-              Calculez {tex`f_x(2, 1)`}et {tex`f_y(2, 1)`} pour
+              Calculez {tex`f_x(2, 1)`} et {tex`f_y(2, 1)`} pour
               {tex`
                 f(x, y) \defeq x^3 + x^2 y^3 - 2y^2
               `}
