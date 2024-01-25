7dea3e4e48890be38ac407e35cd607ab20a7e2fb
Khoi Nguyen <khoi@nguyen.me.uk>
2024-01-25 17:26:12 +0100

    Improve BA lecture on HTTP

--- src/routes/teaching/ba-web-architecture/2-single-page-applications.tsx
+++ src/routes/teaching/ba-web-architecture/2-single-page-applications.tsx
@@ -8,6 +8,77 @@ const meta: Metadata = {
 export default () => {
   return (
     <Slideshow meta={meta}>
+      <Slide title="Structure du chapitre">
+        <ol>
+          <li>
+            Fonctionnement de <Abbr key="HTTP" />
+          </li>
+          <li>
+            <Abbr key="MPA" />
+          </li>
+          <li>
+            <Abbr key="SPA" />
+          </li>
+          <li>
+            <Abbr key="SSR" />
+          </li>
+          <li>État de l'art (si on a le temps)</li>
+        </ol>
+        <Idea>
+          <ul>
+            <li>
+              Les besoins business poussent vers plus de performance et plus d'interactivité{' '}
+              {tex`\to`} <Abbr key="SPA" />.
+            </li>
+            <li>
+              Le code d'une <Abbr key="SPA" /> doit altérer le comportement du navigateur et faire
+              les requêtes <Abbr key="HTTP" /> lui-même.
+            </li>
+            <li>
+              Le but du <Abbr key="SSR" /> est de mitiger les désavantages des <Abbr key="SPA" />.
+            </li>
+          </ul>
+        </Idea>
+      </Slide>
+      <Slide title="Fonctionnement">
+        <Question>
+          <p>Que se passe-t-il lorsque le navigateur demande une page?</p>
+        </Question>
+        <p>Par exemple, supposons qu'on veuille charger</p>
+        {tex`
+          \underbrace{\texttt{https://}}_{\text{Protocole}}
+          \overbrace{\texttt{instagram.com}}^{\text{Nom de domaine}}
+          \underbrace{\texttt{/kimkardashian}}_{\text{Chemin}}
+        `}
+        {mermaid`
+          sequenceDiagram
+            participant browser as Navigateur
+            participant dns as Serveur DNS<br />8.8.8.8
+            participant server as Serveur instagram<br />179.60.195.174
+            browser ->> dns: Adresse de instagram.com?
+            dns ->> browser: 179.60.195.174
+            browser ->> server: GET /kimkardashian
+            server ->> browser: Réponse avec code HTML
+        `}
+        <p>
+          Le navigateur va ensuite faire des requêtes pour chaque image, fichier <Abbr key="CSS" />{' '}
+          ou script JavaScript.
+        </p>
+      </Slide>
+      <Slide title={() => <Abbr key="URL" />}>
+        <Figure src="URI_syntax_diagram.svg" alt="Syntaxe d'une URL" />
+      </Slide>
+      <Slide
+        title={() => (
+          <>
+            Requêtes <Abbr key="HTTP" />
+          </>
+        )}
+      >
+        <p>
+          Les échanges <Abbr key="HTTP" /> se font en texte clair.
+        </p>
+      </Slide>
       <Slide
         title={() => (
           <>
