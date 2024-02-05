80942589df4fa77cfbfb66d63f3e4fcf26a1d387
Khoi Nguyen <khoi@nguyen.me.uk>
2024-02-05 23:52:27 +0100

    Add typescript slide

--- src/routes/teaching/web-architecture/01-react.tsx
+++ src/routes/teaching/web-architecture/01-react.tsx
@@ -113,6 +113,37 @@ export default () => {
           <li>Make good architecture choices for hybrid development</li>
         </ol>
       </Slide>
+      <Slide title="TypeScript" columns>
+        <Iframe src="https://typescriptlang.org" />
+        <ul>
+          <li>First release: 2012</li>
+          <li>
+            Developed by <a href="https://microsoft.com">Microsoft</a>
+          </li>
+          <li>
+            Superset of JavaScript that adds <strong>type annotations</strong>
+          </li>
+          <li>
+            Filename extensions: <code>.ts</code>, <code>.tsx</code>
+          </li>
+        </ul>
+      </Slide>
+      <Slide title="TypeScript">
+        {ts.hl`
+          type Penguin = {
+            name: string;
+            age: number;
+            tie: boolean;
+          }
+
+          const tuxie: Penguin = {
+            name: 'Tuxie',
+            age: 8,
+            tie: true,
+          }
+        `}
+        <p>We shall see that using TypeScript will give us very helpful Intellisense.</p>
+      </Slide>
       <Slide title="Why should we use JS frameworks?">
         <Question>
           <p>Why should we use JS frameworks?</p>
