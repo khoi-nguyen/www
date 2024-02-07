7c527a3f5a52bb77d6c673dbbc016b6f04dabecb
Khoi Nguyen <khoi@nguyen.me.uk>
2024-02-07 14:26:39 +0100

    Fix errors for French slides

--- src/components/Environment.tsx
+++ src/components/Environment.tsx
@@ -22,12 +22,13 @@ interface SpecificEnvironmentProps {
 
 interface EnvironmentProps extends SpecificEnvironmentProps {
   name: string
+  class?: string
 }
 
 export function Environment(props: EnvironmentProps) {
   return (
     <div class="environment">
-      <h3 class={props.name.toLowerCase()}>
+      <h3 class={props.class}>
         <Show when={props.icon}>
           {(icon) => (
             <>
@@ -49,71 +50,73 @@ export function Environment(props: EnvironmentProps) {
 
 export function Corollary(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Corollaire' : 'Corollary')
-  return <Environment name={name()} icon={faBook} {...props} />
+  return <Environment class="corollary" name={name()} icon={faBook} {...props} />
 }
 
 export function Definition(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Définition' : 'Definition')
-  return <Environment name={name()} icon={faBook} {...props} />
+  return <Environment class="definition" name={name()} icon={faBook} {...props} />
 }
 
 export function Example(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Exemple' : 'Example')
-  return <Environment name={name()} icon={faPen} {...props} />
+  return <Environment class="example" name={name()} icon={faPen} {...props} />
 }
 
 export function Exercise(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Exercice' : 'Exercise')
-  return <Environment name={name()} icon={faPenToSquare} {...props} />
+  return <Environment class="exercise" name={name()} icon={faPenToSquare} {...props} />
 }
 
 export function Idea(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Idée' : 'Idea')
-  return <Environment name={name()} icon={faLightbulb} {...props} />
+  return <Environment class="idea" name={name()} icon={faLightbulb} {...props} />
 }
 
 export function Information(props: SpecificEnvironmentProps) {
-  return <Environment name="Information" icon={faInfoCircle} {...props} />
+  return <Environment class="information" name="Information" icon={faInfoCircle} {...props} />
 }
 
 export function Instruction(props: SpecificEnvironmentProps) {
-  return <Environment name="Instruction" icon={faScrewdriverWrench} {...props} />
+  return (
+    <Environment class="instruction" name="Instruction" icon={faScrewdriverWrench} {...props} />
+  )
 }
 
 export function Interpretation(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Interprétation' : 'Interpretation')
-  return <Environment name={name()} icon={faImage} {...props} />
+  return <Environment class="interpretation" name={name()} icon={faImage} {...props} />
 }
 
 export function Problem(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Problème' : 'Problem')
-  return <Environment name={name()} icon={faBook} {...props} />
+  return <Environment class="problem" name={name()} icon={faBook} {...props} />
 }
 
 export function Proposition(props: SpecificEnvironmentProps) {
-  return <Environment name="Proposition" icon={faBook} {...props} />
+  return <Environment class="proposition" name="Proposition" icon={faBook} {...props} />
 }
 
 export function Recall(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Rappel' : 'Recall')
-  return <Environment name={name()} icon={faHistory} {...props} />
+  return <Environment class="recall" name={name()} icon={faHistory} {...props} />
 }
 
 export function Remark(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Remarque' : 'Remark')
-  return <Environment name={name()} icon={faBook} {...props} />
+  return <Environment class="remark" name={name()} icon={faBook} {...props} />
 }
 
 export function Question(props: SpecificEnvironmentProps) {
-  return <Environment name="Question" icon={faCircleQuestion} {...props} />
+  return <Environment class="question" name="Question" icon={faCircleQuestion} {...props} />
 }
 
 export function Quote(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Citation' : 'Quote')
-  return <Environment name={name()} icon={faQuoteLeft} {...props} />
+  return <Environment class="quote" name={name()} icon={faQuoteLeft} {...props} />
 }
 
 export function Theorem(props: SpecificEnvironmentProps) {
   const name = () => (lang() === 'fr' ? 'Théorème' : 'Theorem')
-  return <Environment name={name()} icon={faBook} {...props} />
+  return <Environment class="theorem" name={name()} icon={faBook} {...props} />
 }
