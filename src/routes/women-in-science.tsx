53f97605fe2e99ad34f056706fe3460d4ec8ae58
Khoi Nguyen <khoi@nguyen.me.uk>
2024-02-07 18:42:36 +0100

    Add explanatory slide

--- src/routes/women-in-science.tsx
+++ src/routes/women-in-science.tsx
@@ -7,7 +7,26 @@ const meta: Metadata = {
 export default function () {
   return (
     <Slideshow meta={meta} autoSlide={30 * 1000} hideTitleSlide hideBoards>
-      <Slide title="Journée internationale des femmes et des filles en science"></Slide>
+      <Slide title="Journée internationale des femmes et des filles en science" columns>
+        <div>
+          <p>
+            Le <strong>11 février</strong> est une <strong>journée internationale</strong> dont le
+            but est d'accroître la participation des femmes et des filles dans les domaines
+            scientifiques.
+          </p>
+          <Figure src="women-in-stem.avif" alt="Women in sciences" width={850} />
+        </div>
+        <div>
+          <Figure src="women-in-stem.png" alt="Women in Sciences by degree level">
+            <p>Proportion des femmes dans les domaines STEM par niveau de diplôme.</p>
+          </Figure>
+          <h4>Cette semaine à l'ECAM</h4>
+          <p>
+            Pour célébrer cette semaine, nous avons décider de mettre en avant certaines femmes qui
+            ont changé l'histoire des sciences et technologies.
+          </p>
+        </div>
+      </Slide>
       <Slide title="Emmy Noether" columns>
         <div class="is-4">
           <Figure src="noether.jpg" alt="Emmy Noether" width={400}>
