51c74bef2c4ce3d2fc8089ee481aff999965d46e
Khoi Nguyen <khoi@nguyen.me.uk>
2024-03-08 13:38:54 +0100

    Prepare two lectures

--- src/routes/teaching/multivariable-calculus/3-multiple-integrals.tsx
+++ src/routes/teaching/multivariable-calculus/3-multiple-integrals.tsx
@@ -253,6 +253,293 @@ export default function () {
       <Slide title="Exercices">
         <Iframe src="/calculus/15.2.pdf" />
       </Slide>
+      <Slide title="Intégrales doubles en coordonnées polaires">
+        <Figure src="polar-coordinates.png" alt="Élément d'aire en coordonnées polaires" />
+        <Proposition>
+          {tex`
+            \iint_R f(x, y) \dd A
+            = \int_\alpha^\beta \int_a^b f(r \cos \theta, r \sin \theta) r \dd r \dd \theta,
+          `}
+          <p>
+            où{' '}
+            {tex`R \defeq \{ (r \cos \theta, r\sin \theta) : r \in [a, b], \theta \in [\alpha, \beta] \}`}
+          </p>
+        </Proposition>
+      </Slide>
+      <Slide title="Intégrales doubles en coordonnées polaires">
+        {tex`
+          \iint_R f(x, y) \dd A
+          = \int_\alpha^\beta \int_a^b f(r \cos \theta, r \sin \theta) r \dd r \dd \theta,
+        `}
+        <Question>
+          <p>Quand employer les coordonnées polaires?</p>
+        </Question>
+        <Remark>
+          <p>N'oubliez pas le facteur {tex`r`} supplémentaire!</p>
+          {tex`
+            \boxed{
+              \dd A = r \dd r \dd \theta
+            }
+          `}
+        </Remark>
+      </Slide>
+      <Slide title="Intégration en coordonnées polaires: exemple">
+        <Example>
+          <p>Évaluez l'intégrale</p>
+          {tex`
+            \iint_R 3x + 4y^2 \dd A,
+            \qquad R = \{(x, y) : y \geq 0, 1 \leq x^2 + y^2 \leq 4 \}
+          `}
+        </Example>
+        {tex`
+          \sin^2 \theta = \frac {1 - \cos 2 \theta} 2
+        `}
+        {py.jupyter`
+          from sympy import *
+          x, y, r, theta = symbols("x y r theta")
+          f = 3 * x + 4 * y**2
+          integrand = f.subs({x: r*cos(theta), y: r*sin(theta)}) * r
+          integrate(integrand, (r, 1, 2), (theta, 0, pi))
+        `}
+      </Slide>
+      <Slide title="Intégrale en coordonnées polaires: exemple">
+        <Example>
+          <p>
+            Trouvez le volume du solide délimité par le plan {tex`z = 0`} et le paraboloïde{' '}
+            {tex`z = 1 - x^2 - y^2`}.
+          </p>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          r, theta = symbols("r theta")
+          x, y = r * cos(theta), r * sin(theta)
+          integrate((1 - x**2 - y**2) * r, (r, 0, 1), (theta, 0, 2*pi))
+        `}
+        <Remark>
+          <p>Si on avait essayé d'intégrer en {tex`(x, y)`}, nous aurions eu</p>
+          {tex`
+            \int_{-1}^1 \int_{-\sqrt{1 - x^2}}^{\sqrt{1 - x^2}} 1 - x^2 - y^2 \dd y \dd x
+          `}
+        </Remark>
+      </Slide>
+      <Slide title="Un exemple un peu plus complexe">
+        {py.plot`
+          plt.axes(projection = 'polar')
+          theta = np.arange(-np.pi/4, np.pi/4, 0.01)
+          r = np.cos(2 * theta)
+          plt.polar(theta, r)
+        `}
+        <Example>
+          <p>
+            Utilisez une intégrale double pour trouver l'aire comprise entre une boucle de{' '}
+            {tex`r = \cos 2\theta`}
+          </p>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          r, theta = symbols("r theta")
+          integrate(r, (r, 0, cos(2*theta)), (theta, -pi/4, pi/4))
+        `}
+      </Slide>
+      <Slide title="Un autre exemple">
+        <Example>
+          <p>
+            Trouvez le volume du solide qui se trouve en dessous du paraboloïde {tex`z = x^2 + y^2`}
+            , au-dessus du plan {tex`xy`} et à l'intérieur du cylindre {tex`x^2 + y^2 = 2x.`}
+          </p>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          r, theta = symbols("r theta")
+          integrate(r**3, (r, 0, 2 * cos(theta)), (theta, -pi/2, pi/2))
+        `}
+      </Slide>
+      <Slide title="Applications des intégrales doubles">
+        <Definition title="Densité">
+          {tex`
+            \overbrace{\rho(x, y)}^{\text{densité}}
+            = \lim \frac {\Delta m} {\Delta A}
+          `}
+        </Definition>
+        {tex`
+          m \approx \sum_i \sum_j \rho(x_{ij}, y_{ij}) \Delta A
+        `}
+        <p>Masse et densité sont reliées par la formule</p>
+        {tex`
+          \boxed{
+            m = \iint_D \rho(x, y) \dd A
+          }
+        `}
+      </Slide>
+      <Slide title="Exemple: charge">
+        <Example>
+          <p>
+            La charge est distribuée sur le triangle formés par les points{' '}
+            {tex`(0, 1), (1, 0), (1, 1)`} avec la densité de charge suivante
+          </p>
+          {tex`
+            \sigma (x, y) = x y
+          `}
+          <p>Calculez la charge totale</p>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y = symbols("x y")
+          integrate(x * y, (y, 1 - x, 1), (x, 0, 1))
+        `}
+      </Slide>
+      <Slide title="Moments et centres de masse">
+        <Recall>
+          {tex`
+            \text{moyenne}
+            = \frac {\displaystyle \iint_E f(x, y) \rho(x, y) \dd A} {\displaystyle \iint_E \rho(x, y) \dd A},
+            \quad \rho(x, y) \defeq 1
+          `}
+        </Recall>
+        <p>
+          De manière similaire, le centre de masse est une moyenne du vecteur position {tex`\vec r`}{' '}
+          pondérée par la densité de masse.
+        </p>
+        {tex`
+          \text{centre de masse}
+          = \frac {\displaystyle \iint_E \vec r \rho(\vec r) \dd A(\vec r)} {
+            \displaystyle \underbrace{\iint_E \rho(\vec r) \dd A(\vec r)}_{\text{masse}}
+          }
+        `}
+      </Slide>
+      <Slide title="Exemple: centre de masse">
+        <Example>
+          <p>
+            Trouvez la masse et le centre de masse d'une lamelle triangulaire dont les sommets sont{' '}
+            {tex`(0, 0)`}, {tex`(1, 0)`}, {tex`(0, 2)`} sachant que la densité est
+          </p>
+          {tex`
+            \rho(x, y) = 1 + 3x + y
+          `}
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y = symbols("x y")
+          rho = 1 + 3 * x + y
+          limits = [(y, 0, 2 - 2*x), (x, 0, 1)]
+          m = integrate(rho, *limits)
+          M_y = integrate(x * rho, *limits)
+          M_x = integrate(y * rho, *limits)
+          1/m * Matrix([M_y, M_x])
+        `}
+      </Slide>
+      <Slide title="Exemple: centre de masse en coordonnées polaires">
+        <Example>
+          <p>
+            La densité d'une lamelle est proportionnelle à la distance au centre du cercle. Trouvez
+            son centre de masse
+          </p>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          r, theta, a, C = symbols("r theta a C")
+          rho = C * r
+          limits = [(r, 0, a), (theta, 0, pi)]
+          m = integrate(rho * r, *limits)
+          M_y = integrate(r**2 * cos(theta) * rho, *limits)
+          M_x = integrate(r**2 * sin(theta) * rho, *limits)
+          1/m * Matrix([M_y, M_x])
+        `}
+      </Slide>
+      <Slide title="Intégrales triples">
+        <Example>
+          {tex`
+            \iiint_B x y z^2 \dd V,
+            \quad B \defeq \{
+              (x, y, z) : 0 \leq x \leq 1, -1 \leq y \leq 2, 0 \leq z \leq 3
+            \}
+          `}
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y, z = symbols("x y z")
+          integrate(x*y*z**2, (x, 0, 1), (y, -1, 2), (z, 0, 3))
+        `}
+      </Slide>
+      <Slide title="Intégrale triple: ordre d'intégration">
+        <Example>
+          {tex`
+            \iiint_E z \dd V
+          `}
+          <p>où {tex`E`} est le solide délimité par les plans</p>
+          {tex`
+            x = 0, \quad y = 0, \quad z = 0, \quad x + y + z = 1
+          `}
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y, z = symbols("x y z")
+          integrate(z, (z, 0, 1 - x - y), (y, 0, 1 - x), (x, 0, 1))
+        `}
+      </Slide>
+      <Slide title="Intégrale triple: ordre d'intégration">
+        <Example>
+          {tex`
+            \iiint_E \sqrt {x^2 + z^2} \dd V
+          `}
+          <p>
+            où {tex`E`} est la région délimitée par le paraboloïde {tex`y = x^2 + z^2`} et le plan{' '}
+            {tex`y = 4`}.
+          </p>
+        </Example>
+        {tex`
+          \iiint_E \sqrt {x^2 + z^2} \dd V
+          &= \iint_{D} \left[ \int_{x^2 + z^2}^4 \sqrt{x^2 + z^2} \dd y \right] \dd A\\
+          &= \iint_D (4 - x^2 - z^2) \underbrace{\sqrt {x^2 + z^2}}_r \dd A
+        `}
+        {py.jupyter`
+          from sympy import *
+          x, y, z = symbols("x y z")
+          integrate(4 * r**2 - r**4, (r, 0, 2), (theta, 0, 2 * pi))
+        `}
+      </Slide>
+      <Slide title="Application des intégrales triples">
+        <Example>
+          <p>
+            Utilisez une intégrale triple pour trouver le <strong>volume</strong> du tétrahèdre
+            délimité par les plans
+          </p>
+          {tex`
+            x + 2y + z = 2,\quad
+            x = 2y,\quad
+            x = 0,\quad
+            z = 0
+          `}
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y, z = symbols("x y z")
+          integrate(1, (z, 0, 2 - x - 2*y), (y, x/2, 1 - x/2), (x, 0, 1))
+        `}
+      </Slide>
+      <Slide title="Application: centre de masse">
+        {tex`
+          m = \iiint_E \rho(x, y, z) \dd V
+          \implies
+          \text{centre de masse} \defeq \frac 1 m \iiint_E \rho(\vec r) \vec r \dd V
+        `}
+        <Example>
+          <p>
+            Trouvez le centre de masse du solide uniorme délimité par le cylindre parabolique{' '}
+            {tex`x = y^2`} et les plans {tex`x = z`}, {tex`z = 0`}, {tex`x = 1`}.
+          </p>
+        </Example>
+        {py.jupyter`
+          from sympy import *
+          x, y, z, rho = symbols("x y z rho")
+          limits = [(z, 0, x), (x, y**2, 1), (y, -1, 1)]
+          m = integrate(rho, *limits)
+          M_yz = integrate(x * rho, *limits)
+          M_xy = integrate(z * rho, *limits)
+          M_xz = integrate(y * rho, *limits)
+          Matrix([M_yz / m, M_xz / m, M_xy / m])
+        `}
+      </Slide>
     </Slideshow>
   )
 }
