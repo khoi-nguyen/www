e327505f175d00e64d548a380c883828db9b2822
Khoi Nguyen <khoi@nguyen.me.uk>
2024-03-20 17:16:42 +0100

    From SharedWorker to Worker

--- src/lib/pyodide.worker.ts
+++ src/lib/pyodide.worker.ts
@@ -1,4 +1,3 @@
-/// <reference path="../../node_modules/@types/sharedworker/index.d.ts" />
 import dedent from 'dedent-js'
 import { loadPyodide } from 'pyodide'
 import type { PyodideInterface } from 'pyodide'
@@ -26,67 +25,64 @@ async function load() {
 }
 const loadPromise = load()
 
-onconnect = (e: MessageEvent) => {
-  const port = e.ports[0]
+// Add a message event listener
+self.onmessage = async (event: MessageEvent<MessageToWorker>) => {
+  let format = 'string'
+  let code = event.data.code
+  const uid = event.data.uid
 
-  port.onmessage = async (event: MessageEvent<MessageToWorker>) => {
-    let format = 'string'
-    let code = event.data.code
-    const uid = event.data.uid
+  await loadPromise
+  await pyodide.loadPackagesFromImports(code)
 
-    await loadPromise
-    await pyodide.loadPackagesFromImports(code)
-
-    // Patches
-    if (code.includes('import matplotlib')) {
-      await pyodide.runPythonAsync(`import os\nos.environ["MPLBACKEND"] = "AGG"`)
-      code = dedent`
-        import base64
-        import io
-        import matplotlib.pyplot as plt
-        ${code}
-        image_data = io.BytesIO()
-        plt.savefig(image_data, format='png')
-        plt.close()
-        image_data.seek(0)
-        base64_data = base64.b64encode(image_data.getvalue()).decode()
-        f'data:image/png;base64,{base64_data}'
-      `
-      format = 'matplotlib'
-    }
-
-    const lines = code.split('\n')
-    const lastLine = lines[lines.length - 1]
-    const assignment = /^(\b\w+\b)\s*=[^=]/
-    const match = lastLine.match(assignment)
-    if (match) {
-      code += `\n${match[1]}`
-    }
-
-    let output
-    try {
-      output = await pyodide.runPythonAsync(code)
-      if (output && output._repr_latex_ !== undefined) {
-        output = output._repr_latex_()
-        output = output.substr(1, output.length - 2)
-        format = 'latex'
-      } else if (output.type === 'list' && output.toJs()[0]._repr_latex_) {
-        const list = output.toJs()
-        output = list
-          .map((element: PyProxy) => {
-            const latex = element._repr_latex_()
-            return latex.substring(1, latex.length - 1)
-          })
-          .join(',')
-        output = `\\left[${output}\\right]`
-        format = 'latex'
-      } else {
-        output = String(output)
-      }
-    } catch (error) {
-      output = error
-      format = 'error'
-    }
-    port.postMessage({ output, uid, format } as MessageFromWorker)
+  // Patches
+  if (code.includes('import matplotlib')) {
+    await pyodide.runPythonAsync(`import os\nos.environ["MPLBACKEND"] = "AGG"`)
+    code = dedent`
+      import base64
+      import io
+      import matplotlib.pyplot as plt
+      ${code}
+      image_data = io.BytesIO()
+      plt.savefig(image_data, format='png')
+      plt.close()
+      image_data.seek(0)
+      base64_data = base64.b64encode(image_data.getvalue()).decode()
+      f'data:image/png;base64,{base64_data}'
+    `
+    format = 'matplotlib'
   }
+
+  const lines = code.split('\n')
+  const lastLine = lines[lines.length - 1]
+  const assignment = /^(\b\w+\b)\s*=[^=]/
+  const match = lastLine.match(assignment)
+  if (match) {
+    code += `\n${match[1]}`
+  }
+
+  let output
+  try {
+    output = await pyodide.runPythonAsync(code)
+    if (output && output._repr_latex_ !== undefined) {
+      output = output._repr_latex_()
+      output = output.substr(1, output.length - 2)
+      format = 'latex'
+    } else if (output.type === 'list' && output.toJs()[0]._repr_latex_) {
+      const list = output.toJs()
+      output = list
+        .map((element: PyProxy) => {
+          const latex = element._repr_latex_()
+          return latex.substring(1, latex.length - 1)
+        })
+        .join(',')
+      output = `\\left[${output}\\right]`
+      format = 'latex'
+    } else {
+      output = String(output)
+    }
+  } catch (error) {
+    output = error
+    format = 'error'
+  }
+  self.postMessage({ output, uid, format } as MessageFromWorker)
 }
