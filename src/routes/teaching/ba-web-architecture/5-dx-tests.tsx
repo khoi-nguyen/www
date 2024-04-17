c1c8ab759e059ea3113d67056c4a46d73ce03846
Khoi Nguyen <khoi@nguyen.me.uk>
2024-04-17 14:27:52 +0200

    Minor changes

--- src/routes/teaching/ba-web-architecture/5-dx-tests.tsx
+++ src/routes/teaching/ba-web-architecture/5-dx-tests.tsx
@@ -8,9 +8,26 @@ const meta: Metadata = {
 export default function () {
   return (
     <Slideshow meta={meta}>
-      <Slide title="Tests"></Slide>
+      <Slide title="Tests">
+        <p>Il existe plusieurs types de tests</p>
+        <ul>
+          <li>
+            Tests <strong>unitaires</strong>: concerne une petite portion de code (unité)
+          </li>
+          <li>
+            Tests d'<strong>intégration</strong>
+          </li>
+          <li>
+            Tests <strong>end-to-end</strong>: fonctionnalité et performance de l'application
+            entière en simulant des scénarios réels
+          </li>
+        </ul>
+      </Slide>
       <Slide title="Tests unitaires">
-        {js.jupyter`
+        <Example>
+          <p>Voici un exemple de test unitaire</p>
+        </Example>
+        {js.hl`
           import { describe, test, expect } from 'vitest'
 
           describe("My first group of tests", function () {
