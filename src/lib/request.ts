8c3b6ac1d2ba8e88a298b00a1fc2c32f458b70df
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-02 12:22:13 +0200

    Use right API depending on context

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,12 +1,15 @@
-import { ResultOf, VariablesOf } from '@graphql-typed-document-node/core'
+import type { ResultOf, VariablesOf } from '@graphql-typed-document-node/core'
 import { request as gRequest } from 'graphql-request'
+
+const url =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.nguyen.me.uk/graphql'
+    : 'http://localhost:8000/graphql'
 
 type Args = Parameters<typeof gRequest>
 export function request<T extends Args[1]>(
   query: T,
   variables: VariablesOf<T>,
 ): Promise<ResultOf<T>> {
-  return gRequest('http://localhost:8000/graphql', query, variables as Args[2]) as Promise<
-    ResultOf<T>
-  >
+  return gRequest(url, query, variables as Args[2]) as Promise<ResultOf<T>>
 }
