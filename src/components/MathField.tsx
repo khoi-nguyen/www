91bd9dfc9b9d672b583e0877b30e462d787d50e3
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-03 13:14:16 +0200

    Add support for value

--- src/components/MathField.tsx
+++ src/components/MathField.tsx
@@ -2,8 +2,10 @@ import type { MathfieldElement } from 'mathlive'
 
 interface MathFieldProps {
   defaultValue?: string
-  onChange: (newValue?: string) => void
-  onInput: (newValue?: string) => void
+  onChange?: (newValue?: string) => void
+  onInput?: (newValue?: string) => void
+  readOnly?: boolean
+  value?: string
 }
 
 export default function MathField(props: MathFieldProps) {
@@ -14,23 +16,32 @@ export default function MathField(props: MathFieldProps) {
     if (!field) {
       const mathlive = await import('mathlive')
       field = new mathlive.MathfieldElement({})
+      field.value = props.value || ''
+      field.readOnly = props.readOnly || false
     }
   }
 
   createEffect(async () => {
     await setup()
     field.style = 'min-width: 250px;'
-    if (props.onInput) {
-      field.addEventListener('input', () => {
+    field.addEventListener('input', () => {
+      if (props.onInput) {
         props.onInput(field.value)
-      })
-    }
-    if (props.onChange) {
-      field.addEventListener('change', () => {
+      }
+    })
+    field.addEventListener('change', () => {
+      if (props.onChange) {
         props.onChange(field.value)
-      })
-    }
+      }
+    })
     container.appendChild(field)
+  })
+
+  createEffect(async () => {
+    await setup()
+    if (props.value) {
+      field.value = props.value
+    }
   })
 
   return <div ref={container!} />
