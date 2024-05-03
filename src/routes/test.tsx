4349c79f97cb81e14aa0ef4f1e1cb9cabbc4cbae
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-03 13:34:54 +0200

    Add a test for a numeric poll

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -6,6 +6,8 @@ const meta: Metadata = {
 export default function () {
   return (
     <Page meta={meta}>
+      <p>What is the area of the circle of radius {tex`r`}?</p>
+      <NumericPoll id="circle-area" answer="\pi r^2" />
       <Factorise />
       <Differentiate expr="x^2 - 5x + 6" />
       <Differentiate expr={'\\sin(4x^2+3x)'} />
