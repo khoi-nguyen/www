d842db47539556aaf47ae88918d5b3e45f1ed7df
Khoi Nguyen <khoi@nguyen.me.uk>
2024-05-03 14:13:57 +0200

    Add example with placeholders

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -8,6 +8,13 @@ export default function () {
     <Page meta={meta}>
       <p>What is the area of the circle of radius {tex`r`}?</p>
       <NumericPoll id="circle-area" answer="\pi r^2" />
+      <p>Write {tex`0.03`} in standard form</p>
+      <NumericPoll
+        id="standard-form-0.03"
+        answer="0.03"
+        defaultValue="\placeholder[mantissa]{} \times 10^{\placeholder[power]{}}"
+        readOnly
+      />
       <Factorise />
       <Differentiate expr="x^2 - 5x + 6" />
       <Differentiate expr={'\\sin(4x^2+3x)'} />
